82c824e0c036366da41e951b7e0bb301b2f95da7
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-04 12:18:09 +0100

    added yeld javascript

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -39,5 +39,6 @@
         <script src="{{ asset('js/vendor.js') }}" defer></script>
         <script src="{{ asset('js/app.js') }}" defer></script>
 
+        @yield('javascript')
 </body>
 </html>
--- blog/resources/views/posts/edit.blade.php
+++ blog/resources/views/posts/edit.blade.php
@@ -1,9 +1,12 @@
 @extends('posts.layout')
 
 
-@section('content')
+@section('javascript')
+    <script type="text/javascript" src="{{ asset('js/tinymce/tinymce.min.js') }}" ></script>
+@endsection
 
-    <script src="{{ asset('js/tinymce/tinymce.min.js') }}" ></script>
+
+@section('content')
 
     <div class="row">
         <div class="col-lg-12 margin-tb">
@@ -56,50 +59,6 @@
 
     </form>
 
-
-    <script type="text/javascript">
-		// https://www.tiny.cloud/docs/get-started/basic-setup/
-			var editor_config = {
-				selector: 'textarea#bodyTextarea',
-				/*plugins : 'advlist autolink link image lists charmap print preview spellchecker media table',*/
-				plugins: [
-				 "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-				 "searchreplace wordcount visualblocks visualchars code fullscreen",
-				 "insertdatetime media nonbreaking save table contextmenu directionality",
-				 "emoticons template paste textcolor colorpicker textpattern"
-			   ],
-				theme: 'modern',
-				height: 400,
-				toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | blockquote | link image media',
-
-
-
-				path_absolute : "/",
-				relative_urls: false,
-			    file_browser_callback : function(field_name, url, type, win) {
-			      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-			      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-			      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-			      if (type == 'image') {
-			        cmsURL = cmsURL + "&type=Images";
-			      } else {
-			        cmsURL = cmsURL + "&type=Files";
-			      }
-
-			      tinyMCE.activeEditor.windowManager.open({
-			        file : cmsURL,
-			        title : 'Filemanager',
-			        width : x * 0.8,
-			        height : y * 0.8,
-			        resizable : "yes",
-			        close_previous : "no"
-			      });
-			    }
-			};
-
-			tinymce.init(editor_config);
-		</script>
 
 
 @endsection
