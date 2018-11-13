09ca12f4248aed14a1e18233eea2261b16053b8c
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-13 12:25:09 +0100

    created organizer table

---
+++ blog/database/migrations/2018_11_13_112042_create_organizers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('website')->nullable();
+            $table->string('facebook')->nullable();
+            $table->integer('created_by');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizers');
+    }
+}
