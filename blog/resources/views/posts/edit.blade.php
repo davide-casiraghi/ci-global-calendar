afaf9cc5f02fcfc9add9073ebc64fca7b61b4383
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-13 11:44:31 +0100

    create view for teacher done

--- blog/resources/views/posts/edit.blade.php
+++ blog/resources/views/posts/edit.blade.php
@@ -79,7 +79,6 @@
         @csrf
         @method('PUT')
 
-
          <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
---
+++ blog/resources/views/teachers/edit.blade.php
@@ -0,0 +1,106 @@
+@extends('teachers.layout')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Teacher</h2>
+            </div>
+        </div>
+    </div>
+
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+    <form action="{{ route('teachers.update',$teacher->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $teacher->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Country:</strong>
+                    <input type="text" name="country" value="{{ $teacher->country }}" class="form-control" placeholder="Country">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Bio:</strong>
+                    <textarea class="form-control" style="height:150px" name="bio" placeholder="Bio">{{ $teacher->bio }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Year of starting to practice:</strong>
+                    <input type="text" name="year_starting_practice" class="form-control" placeholder="AAAA" value="{{ $teacher->year_starting_practice }}">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Year of starting to teach:</strong>
+                    <input type="text" name="year_starting_teach" class="form-control" placeholder="AAAA" value="{{ $teacher->year_starting_teach }}">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Facebook profile:</strong>
+                    <input type="text" name="facebook" value="{{ $teacher->facebook }}" class="form-control" placeholder="https://...">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Website:</strong>
+                    <input type="text" name="website" value="{{ $teacher->website }}" class="form-control" placeholder="https://...">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row h-100 mt-3">
+            <div class="col-xs-9 col-sm-9 col-md-9 pull-left my-auto">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><strong>Photo</strong></span>
+                    </div>
+                    <input id="thumbnail" class="form-control" type="text" name="image" value="{{ $teacher->image }}">
+                    <span class="input-group-btn">
+                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                            <i class="fa fa-picture-o"></i> Choose
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 pull-right my-auto">
+                <img id="holder" style="width:100%;" src="{{ $teacher->image }}">
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-xs-6 col-sm-6 col-md-6 pull-left">
+                <a class="btn btn-primary" href="{{ route('teachers.index') }}"> Back</a>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 pull-right">
+              <button type="submit" class="btn btn-primary float-right">Submit</button>
+            </div>
+        </div>
+
+    </form>
+
+@endsection
