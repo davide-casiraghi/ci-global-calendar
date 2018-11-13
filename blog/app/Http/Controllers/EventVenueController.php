1b1f5cc925dda08ed1f4aff3c67fd1a6b0a7c0ae
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-13 13:21:51 +0100

    added venue controller

---
+++ blog/app/Http/Controllers/EventVenueController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EventVenue;
+use Illuminate\Http\Request;
+
+class EventVenueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EventVenue  $eventVenue
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EventVenue $eventVenue)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EventVenue  $eventVenue
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EventVenue $eventVenue)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EventVenue  $eventVenue
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EventVenue $eventVenue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EventVenue  $eventVenue
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EventVenue $eventVenue)
+    {
+        //
+    }
+}
