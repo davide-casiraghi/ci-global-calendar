a33a67d50f31bd82a9079bf0fe99f8aeb0f2be67
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-13 14:12:41 +0100

    create view for venues done

---
+++ blog/resources/views/eventVenues/create.blade.php
@@ -0,0 +1,120 @@
+@extends('eventVenues.layout')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Add new event Venue</h2>
+            </div>
+        </div>
+    </div>
+
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+    <form action="{{ route('eventCategories.store') }}" method="POST">
+        @csrf
+
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Description:</strong>
+                    <textarea class="form-control" style="height:150px" name="description" placeholder="Detail" id="bodyTextarea"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Continent:</strong>
+                    <input type="text" name="continent" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Country:</strong>
+                    <input type="text" name="country" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>City:</strong>
+                    <input type="text" name="city" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Address:</strong>
+                    <input type="text" name="address" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Zip code:</strong>
+                    <input type="text" name="zip_code" class="form-control">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Facebook profile:</strong>
+                    <input type="text" name="facebook" class="form-control" placeholder="https://...">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Website:</strong>
+                    <input type="text" name="website" class="form-control" placeholder="https://...">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row h-100 mt-3">
+            <div class="col-xs-9 col-sm-9 col-md-9 pull-left my-auto">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><strong>Image</strong></span>
+                    </div>
+                    <input id="thumbnail" class="form-control" type="text" name="image">
+                    <span class="input-group-btn">
+                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                            <i class="fa fa-picture-o"></i> Choose
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 pull-right my-auto">
+                <img id="holder" style="width:100%;">
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-xs-6 col-sm-6 col-md-6 pull-left">
+                <a class="btn btn-primary" href="{{ route('eventVenues.index') }}"> Back</a>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 pull-right">
+              <button type="submit" class="btn btn-primary float-right">Submit</button>
+            </div>
+        </div>
+
+
+    </form>
+
+
+@endsection
