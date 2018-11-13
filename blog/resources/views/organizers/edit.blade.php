e1d794a013e9c57b464cc8645eaa051cb5abf96d
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-13 12:57:33 +0100

    organizer edit view done

---
+++ blog/resources/views/organizers/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('organizers.layout')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Organizer</h2>
+            </div>
+        </div>
+    </div>
+
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+    <form action="{{ route('organizers.update',$organizer->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $organizer->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Facebook profile:</strong>
+                    <input type="text" name="facebook" value="{{ $organizer->facebook }}" class="form-control" placeholder="https://...">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Website:</strong>
+                    <input type="text" name="website" value="{{ $organizer->website }}" class="form-control" placeholder="https://...">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row h-100 mt-3">
+            <div class="col-xs-9 col-sm-9 col-md-9 pull-left my-auto">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><strong>Image</strong></span>
+                    </div>
+                    <input id="thumbnail" class="form-control" type="text" name="image" value="{{ $organizer->image }}">
+                    <span class="input-group-btn">
+                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                            <i class="fa fa-picture-o"></i> Choose
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 pull-right my-auto">
+                <img id="holder" style="width:100%;" src="{{ $organizer->image }}">
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-xs-6 col-sm-6 col-md-6 pull-left">
+                <a class="btn btn-primary" href="{{ route('organizers.index') }}"> Back</a>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 pull-right">
+              <button type="submit" class="btn btn-primary float-right">Submit</button>
+            </div>
+        </div>
+
+    </form>
+
+@endsection
