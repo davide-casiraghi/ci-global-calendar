a518aa4852b64116d96312dc6cf470d278ed3455
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-18 10:33:39 +0100

    now teachers is perfect

--- blog/resources/views/partials/forms/select-event-teacher.blade.php
+++ blog/resources/views/partials/forms/select-event-teacher.blade.php
@@ -1,8 +1,18 @@
 @section('javascript-document-ready')
     @parent
+
+    {{-- Update the multiple teachers hidden input when a teacher is selected in the bootstrap select --}}
     $('#teacher').change(function(){
         $('#multiple_teachers').val($('#teacher').val());
      });
+
+     {{-- Select the teachers that are already selected --}}
+     var teachersSelected = $('#multiple_teachers').val();
+     if (teachersSelected){
+         var teachersSelectedArray = teachersSelected.split(',');
+         $('#teacher').selectpicker('val', teachersSelectedArray);
+     }
+
 @stop
 
 
