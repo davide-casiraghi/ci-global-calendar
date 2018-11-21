4d2a6b8e5888c54ee60dd2d29c539f92925e10a7
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-21 16:52:17 +0100

    added icons to the main menu
    Former-commit-id: 597e8c7b14bef0c8b8be996dedb6e13a5e5e2733

--- blog/app/Http/Middleware/GenerateMenus.php
+++ blog/app/Http/Middleware/GenerateMenus.php
@@ -16,24 +16,34 @@ class GenerateMenus
     public function handle($request, Closure $next)
     {
         \Menu::make('MyNavBar', function ($menu) {
-            $menu->add('Home');
+            $menu->add('Home')->prepend('<i class="fa fa-home"></i> ');
 
 
 
 
-            $menu->add('search', ['action' => ['EventSearchController@index']]);
+            //$menu->add('search', ['action' => ['EventSearchController@index']]);
 
             /*$menu->add('About', 'posts/about');
                 $menu->about->add('Info', 'info');
                     $menu->info->add('Ciao', 'ciao');*/
 
+            //$menu->add('About',    ['route'  => 'events.index', 'class' => 'eww']);
+
             //$menu->add('About', ['action' => ['PostController@postdata', 'slug' => 'about-us']]);
             $menu->add('About', ['action' => ['PostController@show', 'id' => 8]]);
+                $menu->about->prepend('<i class="fa fa fa-info-circle"></i> ');
+
+
                 $menu->about->add('Terms of use', ['action' => ['PostController@show', 'id' => 19]]);
             //$menu->add('About',    ['route'  => ['posts', 'id' => 1]]);
 
             $menu->add('Get Involved', ['action' => ['PostController@show', 'id' => 16]]);
+                $menu->getInvolved->prepend('<i class="fa fa-users"></i> ');
+
             $menu->add('How to', ['action' => ['PostController@show', 'id' => 20]]);
+                $menu->howTo->prepend('<i class="fa fas fa-question-circle"></i> ');
+
+
 
             /*$menu->add('Admin', 'admin');
                 $menu->admin->add('Posts', 'posts');
