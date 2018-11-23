bae8b8f11b0d03ed2615b9837be0f0b8b3054bfa
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-23 16:12:43 +0100

    menu are styled
    Former-commit-id: 15a69a657e22e975a6f676b0ece0e8f4a4f1bd86

--- blog/app/Http/Middleware/GenerateMenus.php
+++ blog/app/Http/Middleware/GenerateMenus.php
@@ -48,22 +48,35 @@ class GenerateMenus
             if ($user){
                 if($user->isSuperAdmin()||$user->isAdmin()){
                     $menu->add('Manager', ['link' => ['#']]);
+                        $menu->manager->prepend('<i class="fa fas fa-edit"></i> ');
                         $menu->manager->add('Users', ['action' => ['UserController@index']]);
+                            $menu->users->prepend('<i class="fas fa-user-alt"></i> ');
                         $menu->manager->add('Posts', ['action' => ['PostController@index']]);
-                        $menu->manager->add('Categories', ['action' => ['CategoryController@index']]);
+                            $menu->posts->prepend('<i class="far fa-file-alt"></i> ');
+                        $menu->manager->add('Post Categories', ['action' => ['CategoryController@index']]);
+                            $menu->postCategories->prepend('<i class="far fa-tags"></i> ');
                         $menu->manager->add('Events', ['action' => ['EventController@index']]);
+                            $menu->events->prepend('<i class="far fa-calendar-alt"></i> ');
                         $menu->manager->add('Event Categories', ['action' => ['EventCategoryController@index']]);
+                            $menu->eventCategories->prepend('<i class="fas fa-tags"></i> ');
                         $menu->manager->add('Venues', ['action' => ['EventVenueController@index']]);
+                            $menu->venues->prepend('<i class="far fa-map-marker-alt"></i> ');
                         $menu->manager->add('Teachers', ['action' => ['TeacherController@index']]);
+                            $menu->teachers->prepend('<i class="far fa-users"></i> ');
                         $menu->manager->add('Organizers', ['action' => ['OrganizerController@index']]);
+                            $menu->organizers->prepend('<i class="fas fa-users"></i> ');
 
                 }
 
                 if($user->isSuperAdmin()){
                     $menu->add('Settings', ['link' => ['#'],'inactive']);
-                        $menu->settings->add('Countries', ['action' => ['CountryController@index']]);
+                        $menu->settings->prepend('<i class="far fa-cog"></i> ');
                         $menu->settings->add('Continents', ['action' => ['ContinentController@index']]);
+                            $menu->continents->prepend('<i class="fas fa-globe-americas"></i> ');
+                        $menu->settings->add('Countries', ['action' => ['CountryController@index']]);
+                            $menu->countries->prepend('<i class="far fa-globe-americas"></i> ');
                         $menu->settings->add('Background images', ['action' => ['BackgroundImageController@index']]);
+                            $menu->backgroundImages->prepend('<i class="far fa-images"></i> ');
                 }
 
             }
--- blog/resources/views/menus/nav-right-items.blade.php
+++ blog/resources/views/menus/nav-right-items.blade.php
@@ -2,7 +2,7 @@
 {{--  Authentication Links --}}
 @guest
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        <a class="nav-link" href="{{ route('login') }}"><i class="fa fa-sign-in "></i> {{ __('Login') }}</a>
     </li>
     {{-- <li class="nav-item">
         <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -20,7 +20,7 @@
          <a class="dropdown-item" href="{{ route('logout') }}"
             onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();">
-             {{ __('Logout') }}
+             <i class="fa fa-sign-out "></i> {{ __('Logout') }}
          </a>
 
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
