5b8ab3f2b66a27c219f3f1e3b104f8e47b695da1
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-24 15:29:10 +0100

    added menu translations for italian and english
    Former-commit-id: c8aeccc50dabd641ac95b19ee1799af015421b65

---
+++ blog/resources/lang/en/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'about' => 'About',
+    'get_involved' => 'Get Involved',
+    'how_to' => 'How to insert contents',
+    'manager' => 'Manager',
+    'new_event' => 'New event',
+    'my_events' => 'My Events',
+    'my_organizers' => 'My Organizers',
+    'my_teachers' => 'My Teachers',
+    'my_venues' => 'My Venues',
+    'my_profile' => 'My Profile',
+    'create_account' => 'Create Account',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'donate' => 'Donate'
+];
---
+++ blog/resources/lang/it/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'about' => 'A proposito',
+    'get_involved' => 'Partecipa',
+    'how_to_insert_contents' => 'Come inserire contenuti',
+    'manager' => 'Gestione',
+    'new_event' => 'Nuovo Evento',
+    'my_events' => 'I miei eventi',
+    'my_organizers' => 'I miei Organizzatori',
+    'my_teachers' => 'I miei Insegnanti',
+    'my_venues' => 'I miei Luoghi',
+    'my_profile' => 'Il mio Profilo',
+    'create_account' => 'Registrati',
+    'login' => 'Accedi',
+    'logout' => 'Logout',
+    'donate' => 'Donazione'
+];
