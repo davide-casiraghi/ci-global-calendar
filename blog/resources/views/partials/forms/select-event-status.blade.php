2835dde940bc63f497bf4548fc72a1a5cb063757
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-29 10:44:37 +0100

    adding status again
    Former-commit-id: 7e1b3aa520b48c80eac04bd21249922c8c7124ab

--- blog/resources/views/partials/forms/select-event-status.blade.php
+++ blog/resources/views/partials/forms/select-event-status.blade.php
@@ -1,13 +1,7 @@
-@section('javascript')
-    @parent
-    javascript-status
-
-@stop
-
 <div class="form-group">
-    <strong>Status:</strong>
+    <strong>Status: </strong>
     <select name="status" class="form-control">
-        <option value="PUBLISHED" @if(!empty($event->status)) {{  $event->status == 'PUBLISHED' ? 'selected' : '' }} @endif>Published</option>
-        <option value="UNPUBLISHED" @if(!empty($event->status)) {{  $event->status == 'UNPUBLISHED' ? 'selected' : '' }} @endif>Unpublished</option>
+        <option value="2" @if(!empty($event->status)) {{  $event->status == '2' ? 'selected' : '' }} @endif>Published</option>
+        <option value="1" @if(!empty($event->status)) {{  $event->status == '1' ? 'selected' : '' }} @endif>Unpublished</option>
     </select>
 </div>
