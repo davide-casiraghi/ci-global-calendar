e0ed55ec81458782508a788ffae96118ab65cb8c
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-29 18:46:56 +0100

    created the view
    Former-commit-id: db6591bdfa45f74288cb77dc24128b458789b373

--- blog/resources/views/administratorMailForms/contactForm.blade.php
+++ blog/resources/views/administratorMailForms/contactForm.blade.php
@@ -10,44 +10,48 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 mt-3 mb-5">
-            Please write your text in english.
-        </div>
+    <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-           @include('partials.forms.input', [
-                 'title' => 'Name',
-                 'name' => 'name',
-                 'placeholder' => 'Your name'
-           ])
-        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-3 mb-5">
+                Please write your text in english.
+            </div>
 
-       <div class="col-xs-12 col-sm-12 col-md-12">
-           @include('partials.forms.input', [
-                 'title' => 'Email',
-                 'name' => 'email',
-                 'placeholder' => 'Your email'
-           ])
+            <div class="col-xs-12 col-sm-12 col-md-12">
+               @include('partials.forms.input', [
+                     'title' => 'Name',
+                     'name' => 'name',
+                     'placeholder' => 'Your name'
+               ])
+            </div>
+
+           <div class="col-xs-12 col-sm-12 col-md-12">
+               @include('partials.forms.input', [
+                     'title' => 'Email',
+                     'name' => 'email',
+                     'placeholder' => 'Your email'
+               ])
+           </div>
+
+           <div class="col-xs-12 col-sm-12 col-md-12">
+               @include('partials.forms.textarea-plain', [
+                     'title' => 'Message',
+                     'name' => 'message',
+                     'placeholder' => 'Your message'
+               ])
+           </div>
+
        </div>
 
-       <div class="col-xs-12 col-sm-12 col-md-12">
-           @include('partials.forms.textarea-plain', [
-                 'title' => 'Message',
-                 'name' => 'message',
-                 'placeholder' => 'Your message'
-           ])
+       <div class="row mt-5">
+           <div class="col-xs-6 col-sm-6 col-md-6 pull-left">
+               <a class="btn btn-primary" href="{{ route('home') }}"> Back</a>
+           </div>
+           <div class="col-xs-6 col-sm-6 col-md-6 pull-right">
+             <button type="submit" class="btn btn-primary float-right">Send</button>
+           </div>
        </div>
-
-   </div>
-
-   <div class="row mt-5">
-       <div class="col-xs-6 col-sm-6 col-md-6 pull-left">
-           <a class="btn btn-primary" href="{{ route('home') }}"> Back</a>
-       </div>
-       <div class="col-xs-6 col-sm-6 col-md-6 pull-right">
-         <button type="submit" class="btn btn-primary float-right">Send</button>
-       </div>
-   </div>
+   </form>
 
 @endsection
