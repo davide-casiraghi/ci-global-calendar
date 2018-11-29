734c06a37942df51fe4c459350c446336c47dff6
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-29 18:43:23 +0100

    created the view
    Former-commit-id: 4f2f4e431ea047e3f30d9e121124b1cc9bbc6522

--- blog/resources/views/administratorMailForms/contactForm.blade.php
+++ blog/resources/views/administratorMailForms/contactForm.blade.php
@@ -1,8 +1,43 @@
 @extends('backgroundImages.layout')
 
 @section('content')
-ddddd
 
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Contact the administrator</h2>
+            </div>
+        </div>
+    </div>
 
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-3 mb-5">
+            Please write your text in english.
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+           @include('partials.forms.input', [
+                 'title' => 'Name',
+                 'name' => 'name',
+                 'placeholder' => 'Your name'
+           ])
+        </div>
+
+       <div class="col-xs-12 col-sm-12 col-md-12">
+           @include('partials.forms.input', [
+                 'title' => 'Email',
+                 'name' => 'email',
+                 'placeholder' => 'Your email'
+           ])
+       </div>
+
+       <div class="col-xs-12 col-sm-12 col-md-12">
+           @include('partials.forms.textarea-plain', [
+                 'title' => 'Description',
+                 'name' => 'description',
+                 'placeholder' => 'Your message'
+           ])
+       </div>
+   </div>
 
 @endsection
