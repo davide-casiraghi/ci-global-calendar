e06c2950df7786eac936d2a06e3be0b927f22b23
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-30 11:32:35 +0100

    fixed user group selection in user edit
    Former-commit-id: 8f2adac7402a0b7ed9de12d9f864cae429f8ff23

--- blog/app/Http/Controllers/Auth/RegisterController.php
+++ blog/app/Http/Controllers/Auth/RegisterController.php
@@ -69,6 +69,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'country_id' => $data['country_id'],
+            'description' => $data['description'],
         ]);
     }
 
