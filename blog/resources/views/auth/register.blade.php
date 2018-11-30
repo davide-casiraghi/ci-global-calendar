0f6f58e6239da02f43dd3f147e983f6f31f77aef
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-30 17:27:29 +0100

    added info about approval for user that register
    Former-commit-id: a2ff7249aef12acca627ff4a2133bfeaf1927bd7

--- blog/resources/views/auth/register.blade.php
+++ blog/resources/views/auth/register.blade.php
@@ -123,7 +123,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary float-right">
                                     {{ __('Register') }}
                                 </button>
                             </div>
