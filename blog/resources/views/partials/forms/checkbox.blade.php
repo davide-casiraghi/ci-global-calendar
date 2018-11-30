af18c6fc6298cdffb7569b697a36ca33a38145d1
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-11-30 16:55:00 +0100

    now the checkbox show the errors if not checked
    Former-commit-id: 6c8318dc2603021892a732d96039cd90e760bfbe

--- blog/resources/views/partials/forms/checkbox.blade.php
+++ blog/resources/views/partials/forms/checkbox.blade.php
@@ -1,6 +1,11 @@
 <div class="form-group">
     <div class="custom-control custom-checkbox">
-        <input type="checkbox" name="{{$name}}"class="custom-control-input" id="customCheck1">
+        <input type="checkbox" name="{{$name}}"class="custom-control-input{{ $errors->has($name) ? ' is-invalid' : '' }}" id="customCheck1">
         <label class="custom-control-label" for="customCheck1">{{$description}}</label>
     </div>
+    @if ($errors->has($name))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first($name) }}</strong>
+        </span>
+    @endif
 </div>
