026a28fb56bc2da57f875853c8695e4e02bfa57a
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-01 14:55:16 +0100

    created modal, now still redirect when save and still js bugs
    Former-commit-id: cdea95c5044e60f5f3de335b4bef58f260a60f5c

--- blog/resources/views/layouts/modal.blade.php
+++ blog/resources/views/layouts/modal.blade.php
@@ -1,42 +1,13 @@
 
 
-    {{--<title>App Name - @yield('title')</title>--}}
+<div class="container pt-5">
+    @yield('content')
+</div>
 
-    {{--<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@yield('javascript')
 
-    <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- CSS-->
-
-        {{--<link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">}}
-
-        @yield('css')
-</head>
-
- <!-- Laravel use VUE as default - https://stackoverflow.com/questions/41411344/vue-warn-cannot-find-element-app#41411385-->
-    {!! menu('main', 'nav') !!}
-
-<body>
---}}
-    <div class="container pt-5">
-        @yield('content')
-    </div>
-
-
-    <!-- JS -->
-        {{--<script src="{{ asset('js/manifest.js') }}" ></script>
-        <script src="{{ asset('js/vendor.js') }}" ></script>
-        <script src="{{ asset('js/app.js') }}" ></script>--}}
-
-        @yield('javascript')
-
-        <script type="text/javascript">
-            $(document).ready(function(){
-                @yield('javascript-document-ready')
-            });
-        </script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        @yield('javascript-document-ready')
+    });
+</script>
