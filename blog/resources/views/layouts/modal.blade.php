7d837f93bb6332b5094c3e8d965d63df7e694a0a
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-01 14:48:56 +0100

    created modal, now still redirect when save and still js bugs
    Former-commit-id: 4089c1fdf2500950bfd0a0e441ff7993111d57e3

--- blog/resources/views/layouts/modal.blade.php
+++ blog/resources/views/layouts/modal.blade.php
@@ -3,31 +3,28 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title>App Name - @yield('title')</title>
-    {{--<title>{{ config('app.name', 'Laravel') }}</title>--}}
+    {{--<title>App Name - @yield('title')</title>--}}
 
-    <meta charset="utf-8">
+    {{--<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">--}}
 
     <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- CSS-->
-        <!--<link rel="stylesheet" type="text/css" href="/css/vendor.css">
-        <link rel="stylesheet" type="text/css" href="/css/app.css">
-        <link rel="stylesheet" type="text/css" href="/css/custom.css">-->
-        <link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
+
+        {{--<link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">}}
 
         @yield('css')
 </head>
 
-<body> <!-- Laravel use VUE as default - https://stackoverflow.com/questions/41411344/vue-warn-cannot-find-element-app#41411385-->
+ <!-- Laravel use VUE as default - https://stackoverflow.com/questions/41411344/vue-warn-cannot-find-element-app#41411385-->
     {{-- {!! menu('main', 'nav') !!} --}}
 
-
+<body>
 
     <div class="container pt-5">
         @yield('content')
@@ -35,7 +32,7 @@
 
 
     <!-- JS -->
-{{--<script src="{{ asset('js/manifest.js') }}" ></script>
+        {{--<script src="{{ asset('js/manifest.js') }}" ></script>
         <script src="{{ asset('js/vendor.js') }}" ></script>
         <script src="{{ asset('js/app.js') }}" ></script>--}}
 
