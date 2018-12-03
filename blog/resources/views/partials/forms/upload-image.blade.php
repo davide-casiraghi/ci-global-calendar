5f20b95b5565eb0d79af7a8b5e531a4b659ac3ae
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-03 12:18:44 +0100

    changed name to image column to profile_picture
    Former-commit-id: df4be38248c4b196f7f5626442c635a90f8cfc92

--- blog/resources/views/partials/forms/upload-image.blade.php
+++ blog/resources/views/partials/forms/upload-image.blade.php
@@ -13,8 +13,8 @@
 @if(!empty($title))<label for="{{ $name }}">{{ $title }}:</label>@endif
 <div class="form-group">
     <div class="custom-file">
-        <input type="file" class="custom-file-input" id="inputGroupFile04">
-        <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+        <input type="file" name="{{ $name }} "class="custom-file-input">
+        <label class="custom-file-label" for="{{ $name }}">Choose file</label>
     </div>
     {{--<div class="input-group-append">
     <button class="btn btn-outline-secondary" type="button">Button</button>
