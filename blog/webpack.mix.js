c119979802ef4d4948be72aba041a1edc9578f6c
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-03 10:13:54 +0100

    assets directory that contains the scripts and styles has been flattened into the resources directory
    Former-commit-id: 0c6f5675672c685fdb3a54dc0acebc939dbadd4e

--- blog/webpack.mix.js
+++ blog/webpack.mix.js
@@ -26,7 +26,7 @@ let mix = require('laravel-mix');
 
 /* JS (generate: manifest.js, vendor.js, app.js)*/
 /*  jQuery first, then Popper.js (tooltips), then Bootstrap JS */
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
    .extract([
         'jquery',
         //'node_modules/jquery/dist/jquery.js',
@@ -68,4 +68,4 @@ mix.styles([
 ], 'public/css/vendor.css');
 
 /* CSS - Custom - OK */
-mix.sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css');
