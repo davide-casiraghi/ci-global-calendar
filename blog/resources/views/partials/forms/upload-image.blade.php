9be7bbe6248e250172a7a22be0cfcaa9442812f9
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-03 11:58:10 +0100

    created upload image partial
    Former-commit-id: fab4cd0cdde0c66dac74a07f7dece8e73cfcf1e2

---
+++ blog/resources/views/partials/forms/upload-image.blade.php
@@ -0,0 +1,10 @@
+@if(!empty($title))<label for="{{ $name }}">{{ $title }}:</label>@endif
+<div class="form-group">
+    <div class="custom-file">
+        <input type="file" class="custom-file-input" id="inputGroupFile04">
+        <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+    </div>
+    {{--<div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+    </div>--}}
+</div>
