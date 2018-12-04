5b9790143593c062b9b9a0f79afc84d984117e95
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-04 19:18:28 +0100

    removed image from veneue create and edit view
    Former-commit-id: a598f1107e90e4c4c6d6833663020f141ecbe92b

--- blog/resources/views/eventVenues/edit.blade.php
+++ blog/resources/views/eventVenues/edit.blade.php
@@ -105,26 +105,6 @@
                     'value' => $eventVenue->website
                 ])
             </div>
-
-        </div>
-
-        <div class="row h-100 mt-3">
-            <div class="col-xs-9 col-sm-9 col-md-9 pull-left my-auto">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text"><strong>Image</strong></span>
-                    </div>
-                    <input id="thumbnail" class="form-control" type="text" name="image" value="{{ $eventVenue->image }}">
-                    <span class="input-group-btn">
-                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-                            <i class="fa fa-picture-o"></i> Choose
-                        </a>
-                    </span>
-                </div>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3 pull-right my-auto">
-                <img id="holder" style="width:100%;" src="{{ $eventVenue->image }}">
-            </div>
         </div>
 
         <div class="row mt-5">
