dd1669fd352876da845b2065a059a79ba781258a
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-05 16:12:13 +0100

    trying to fix the existing timepicker
    Former-commit-id: 9fe5c2abfb33c70d901e5dd9dc889c7a81b9d5d9

--- blog/resources/views/partials/forms/input-time-start.blade.php
+++ blog/resources/views/partials/forms/input-time-start.blade.php
@@ -5,6 +5,17 @@
 
 <div class="form-group">
     <strong>Time Start:</strong>
+    <div class="input-group">
+        <input id="timepicker_start" name="time_start" type="text" class="form-control">
+        <div class="input-group-append">
+            <span class="input-group-text" id="time-addon-start"><i class="far fa-clock"></i></span>
+        </div>
+    </div>
+</div>
+
+{{--
+<div class="form-group">
+    <strong>Time Start:</strong>
     <div class="input-group bootstrap-timepicker timepicker">
         <input id="timepicker_start" name="time_start" @if(!empty($dateTime['timeStart'])) value="{{ $dateTime['timeStart'] }}" @endif type="text" class="form-control input-small" aria-describedby="time-addon-start">
         <div class="input-group-append">
@@ -12,3 +23,4 @@
         </div>
     </div>
 </div>
+--}}
