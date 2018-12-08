76f0bf92e234a36727e8d8a68499ad2ef9a57351
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-08 05:00:27 +0100

    fixing modal add venue
    Former-commit-id: cd6b7e3b31c51f25ff96c9065e4f0c912ac768f1

--- blog/resources/views/eventVenues/modal.blade.php
+++ blog/resources/views/eventVenues/modal.blade.php
@@ -17,85 +17,83 @@
         @csrf
 
         <div class="row">
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Name:</strong>
-                   <input type="text" name="name" class="form-control" placeholder="Name">
-               </div>
-           </div>
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Description:</strong>
-                   <textarea class="form-control" style="height:150px" name="description" placeholder="Detail" id="bodyTextarea"></textarea>
-               </div>
-           </div>
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Country:</strong>
-                   <select name="country_id" class="selectpicker" data-live-search="true" title="Select country">
-                       @foreach ($countries as $value => $country)
-                           <option value="{{$value}}">{!! $country !!}</option>
-                       @endforeach
-                   </select>
-               </div>
-           </div>
-
-
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>City:</strong>
-                   <input type="text" name="city" class="form-control">
-               </div>
-           </div>
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Address:</strong>
-                   <input type="text" name="address" class="form-control">
-               </div>
-           </div>
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Zip code:</strong>
-                   <input type="text" name="zip_code" class="form-control">
-               </div>
-           </div>
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Facebook profile:</strong>
-                   <input type="text" name="facebook" class="form-control" placeholder="https://...">
-               </div>
-           </div>
-           <div class="col-12">
-               <div class="form-group">
-                   <strong>Website:</strong>
-                   <input type="text" name="website" class="form-control" placeholder="https://...">
-               </div>
-           </div>
-
-       </div>
-
-       <div class="row h-100 mt-3">
-           <div class="col-xs-9 col-sm-9 col-md-9 pull-left my-auto">
-               <div class="input-group">
-                   <div class="input-group-prepend">
-                       <span class="input-group-text"><strong>Image</strong></span>
-                   </div>
-                   <input id="thumbnail" class="form-control" type="text" name="image">
-                   <span class="input-group-btn">
-                       <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-                           <i class="fa fa-picture-o"></i> Choose
-                       </a>
-                   </span>
-               </div>
-           </div>
-           <div class="col-xs-3 col-sm-3 col-md-3 pull-right my-auto">
-               <img id="holder" style="width:100%;">
-           </div>
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'Name',
+                    'name' => 'name',
+                    'placeholder' => 'Name'
+                ])
+            </div>
+            
+            {{-- Show the created by field just to the admin and super admin --}}
+            @if(empty($authorUserId))
+                <div class="col-12">
+                    @include('partials.forms.select', [
+                        'title' => 'Created by',
+                        'name' => 'created_by',
+                        'placeholder' => 'Select owner',
+                        'records' => $users
+                    ])
+                </div>
+            @endif
+            
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'Street',
+                    'name' => 'address',
+                    'placeholder' => ''
+                ])
+            </div>
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'City',
+                    'name' => 'city',
+                    'placeholder' => ''
+                ])
+            </div>
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'State/Province',
+                    'name' => 'state_province',
+                    'placeholder' => ''
+                ])
+            </div>
+            <div class="col-12">
+                @include('partials.forms.select', [
+                      'title' => 'Country',
+                      'name' => 'country_id',
+                      'placeholder' => 'Select country',
+                      'records' => $countries,
+                ])
+            </div>
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'Zip code',
+                    'name' => 'zip_code',
+                    'placeholder' => '',
+                    'value' => ''
+                ])
+            </div>
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'Website',
+                    'name' => 'website',
+                    'placeholder' => 'https://...',
+                    'value' => ''
+                ])
+            </div>
+            <div class="col-12">
+                @include('partials.forms.textarea', [
+                    'title' => 'Description',
+                    'name' => 'description',
+                    'placeholder' => 'Event description'
+                ])
+            </div>
        </div>
 
        <div class="row mt-5">
            <div class="col-6 pull-left">
-               <a class="btn btn-primary" href="{{ route('eventVenues.index') }}"> Back</a>
+               <a class="btn btn-primary" href="{{ route('eventVenues.index') }}">Back</a>
            </div>
            <div class="col-6 pull-right">
              <button type="submit" class="btn btn-primary float-right">Submit</button>
