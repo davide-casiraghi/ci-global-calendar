0a6992e13542a1e68ac472250e09ce59c7b417f2
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-08 14:07:23 +0100

    event cateory index view responsive
    Former-commit-id: c33a907270ec10d86b4fb6de90ec04dacb9d9eae

--- blog/resources/views/eventCategories/index.blade.php
+++ blog/resources/views/eventCategories/index.blade.php
@@ -5,14 +5,13 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Event categories management</h2>
+                <h2>@lang('views.events_category_management')</h2>
             </div>
             <div class="pull-right mt-4 float-right">
-                <a class="btn btn-success" href="{{ route('eventCategories.create') }}"> Create New Category</a>
+                <a class="btn btn-success" href="{{ route('eventCategories.create') }}">@lang('views.create_new_category')</a>
             </div>
         </div>
     </div>
-
 
     @if ($message = Session::get('success'))
         <div class="alert alert-success mt-4">
@@ -21,7 +20,32 @@
     @endif
 
 
-    <table class="table table-bordered mt-4">
+    {{-- List of categories --}}
+    <div class="venuesList my-4">
+        @foreach ($eventCategories as $eventCategory)
+            <div class="row p-1 {{ $loop->index % 2 ? 'bg-light': 'bg-white' }}">
+                <div class="col-12 col-md-6 col-lg-8 py-3 title">
+                    <a href="{{ route('eventCategories.edit',$eventCategory->id) }}">{{ $eventCategory->name }}</a>
+                </div>
+                
+                <div class="col-12 pb-2 action">
+                    <form action="{{ route('eventCategories.destroy',$eventCategory->id) }}" method="POST">
+
+                        {{--<a class="btn btn-info mr-2" href="{{ route('eventCategories.show',$eventCategory->id) }}">@lang('views.view')</a>--}}
+                        <a class="btn btn-primary" href="{{ route('eventCategories.edit',$eventCategory->id) }}">@lang('views.edit')</a>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" class="btn btn-danger float-right">@lang('views.delete')</button>
+                    </form>
+                </div>
+            </div>
+        @endforeach    
+    </div>
+
+    {{-- List of categories --}}
+    {{--<table class="table table-bordered mt-4">
         <tr>
             <th>ID</th>
             <th>Name</th>
@@ -48,7 +72,7 @@
             </td>
         </tr>
         @endforeach
-    </table>
+    </table>--}}
 
 
     {!! $eventCategories->links() !!}
