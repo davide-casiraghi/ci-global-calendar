fb3e93b8cf69b8fcf84168721bf1789b1343c108
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-08 15:37:40 +0100

    responsive background image index view
    Former-commit-id: d4fad60e6148732f3cbb74bc2c8bd5249a1105dd

--- blog/resources/views/backgroundImages/index.blade.php
+++ blog/resources/views/backgroundImages/index.blade.php
@@ -12,43 +12,46 @@
         </div>
     </div>
 
-
     @if ($message = Session::get('success'))
         <div class="alert alert-success mt-4">
             <p>{{ $message }}</p>
         </div>
     @endif
 
-
-    <table class="table table-bordered mt-4">
-        <tr>
-            <th>Title</th>
-            <th>Credits</th>
-            <th width="160">Image</th>
-            <th width="100">Orientation</th>
-            <th width="230">Action</th>
-        </tr>
+    {{-- List of background images --}}
+    <div class="countriesList my-4">
         @foreach ($backgroundImages as $backgroundImage)
-        <tr>
-            <td>{{ $backgroundImage->title }}</td>
-            <td>{{ $backgroundImage->credits }}</td>
-            <td><img src="{{ $backgroundImage->image_src }}" width="150" class="mx-auto d-block"></td>
-            <td>{{  $backgroundImage->orientation == 0 ? 'Horizontal' : 'Vertical' }}</td>
-            <td>
-                <form action="{{ route('backgroundImages.destroy',$backgroundImage->id) }}" method="POST">
+            <div class="row p-1 {{ $loop->index % 2 ? 'bg-light': 'bg-white' }}">
+                <div class="col-12 col-sm-6 col-md-4 py-3 title">
+                    <a href="{{ route('backgroundImages.edit',$backgroundImage->id) }}">{{ $backgroundImage->title }}</a>
+                </div>
+                
+                <div class="col-12 col-sm-3 col-md-2 py-3 code">
+                    <i data-toggle="tooltip" data-placement="top" title="" class="far fa-barcode-alt mr-2" data-original-title="@lang('general.code')"></i>
+                    {{ $backgroundImage->credits }}
+                </div>
+                <div class="col-12 col-sm-3 col-md-3 py-3 code">
+                    <img src="{{ $backgroundImage->image_src }}" width="150" class="mx-auto d-block">
+                </div>
+                <div class="col-12 col-sm-12 col-md-3 py-3 code">
+                    {{  $backgroundImage->orientation == 0 ? 'Horizontal' : 'Vertical' }}
+                </div>
+                
+                <div class="col-12 pb-2 action">
+                    <form action="{{ route('backgroundImages.destroy',$backgroundImage->id) }}" method="POST">
 
-                    <a class="btn btn-info" href="{{ route('backgroundImages.show',$backgroundImage->id) }}">Show</a>
-                    <a class="btn btn-primary" href="{{ route('backgroundImages.edit',$backgroundImage->id) }}">Edit</a>
+                        <a class="btn btn-info mr-2" href="{{ route('backgroundImages.show',$backgroundImage->id) }}">@lang('views.view')</a>
+                        <a class="btn btn-primary" href="{{ route('backgroundImages.edit',$backgroundImage->id) }}">@lang('views.edit')</a>
 
-                    @csrf
-                    @method('DELETE')
+                        @csrf
+                        @method('DELETE')
 
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+                        <button type="submit" class="btn btn-danger float-right">@lang('views.delete')</button>
+                    </form>
+                </div>
+            </div>
+    @endforeach    
+    </div>
 
     {!! $backgroundImages->links() !!}
 
