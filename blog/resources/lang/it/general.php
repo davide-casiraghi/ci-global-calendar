b65e3210e170faf62934c68925d84a1ffbded371
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-08 12:17:48 +0100

    fixed languages of event search view
    Former-commit-id: f00431d19947637024cf263a84a956099fb1ccab

--- blog/resources/lang/it/general.php
+++ blog/resources/lang/it/general.php
@@ -3,10 +3,20 @@
 return [
     'search' => 'Cerca',
     'reset' => 'Resetta',
+    'back' => 'Back',
+    'submit' => 'Submit',
     'category' => 'Categoria',
     'organizers' => 'Organizzatori',
     'teachers' => 'Insegnanti',
     'venue' => 'Luogo',
+    'from' => 'From',
+    'to' => 'To',
+    'by' => 'By',
+    'links' => 'Links',
+    'description' => 'Description',
+    'country' => 'Stato',
     'privacy_policy' => 'For more information, please consult the Privacy Policy.',
-    'accept_terms_of_use' => 'Dichiaro di aver letto e di accettare i Termini e Condizioni d’Uso'
+    'accept_terms_of_use' => 'Dichiaro di aver letto e di accettare i Termini e Condizioni d’Uso',
+    'admin_account_approval' => 'Your account has to be approved by the administrator and it can take some time (days) to get the confirmation. Next time you only need to login and enter your new entry.'
+        
 ];
--- blog/resources/views/events/index.blade.php
+++ blog/resources/views/events/index.blade.php
@@ -16,7 +16,7 @@
             <h2>Events Management</h2>
         </div>
         <div class="col-12 mt-4 mt-sm-0 text-right">
-            <a class="btn btn-success" href="{{ route('events.create') }}"> Create New event</a>
+            <a class="btn btn-success" href="{{ route('events.create') }}">@lang('views.create_new_event')</a>
         </div>
     </div>
 
@@ -30,10 +30,10 @@
     <form class="row searchForm mt-3" action="{{ route('events.index') }}" method="GET">
         @csrf
         <div class="form-group col-12 col-sm-12 col-md-4 col-lg-3 mb-2">
-            <input type="text" name="keywords" class="form-control" placeholder="Search by event name" value="{{ $searchKeywords }}">
+            <input type="text" name="keywords" class="form-control" placeholder="@lang('views.search_by_event_name')" value="{{ $searchKeywords }}">
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
-            <select name="category_id" class="selectpicker" data-live-search="true" title="Search by category">
+            <select name="category_id" class="selectpicker" data-live-search="true" title="@lang('views.filter_by_category')">
                 @foreach ($eventCategories as $value => $eventCategory)
                     {{-- {{ $event->category_id == $value ? 'selected' : '' }} --}}
                     <option value="{{$value}}" {{ $searchCategory == $value ? 'selected' : '' }} >{!! $eventCategory !!} </option>
@@ -41,7 +41,7 @@
             </select>
         </div>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <select name="country_id" class="selectpicker" data-live-search="true" title="Search by country">
+            <select name="country_id" class="selectpicker" data-live-search="true" title="@lang('views.filter_by_country')">
                 @foreach ($countries as $value => $country)
                     <option value="{{$value}}" {{ $searchCountry == $value ? 'selected' : '' }} >{!! $country !!} </option>
                 @endforeach
@@ -94,23 +94,23 @@
                     <a href="{{ route('events.edit',$event->id) }}">{{ $event->title }}</a>
                 </div>
                 <div class="col-6 col-md-3 col-lg-2 pb-2 py-md-2 category">
-                    <i data-toggle="tooltip" data-placement="top" title="" class="fa fa-tag mr-2" data-original-title="Category"></i>
+                    <i data-toggle="tooltip" data-placement="top" title="" class="fa fa-tag mr-2" data-original-title="@lang('general.category')"></i>
                     {{ $eventCategories[$event->category_id] }}
                 </div>
                 <div class="col-6 col-md-3 col-lg-2 pb-2 py-md-2 country">
-                    <i data-toggle="tooltip" data-placement="top" title="" class="far fa-globe-americas mr-2" data-original-title="Country"></i>
+                    <i data-toggle="tooltip" data-placement="top" title="" class="far fa-globe-americas mr-2" data-original-title="@lang('general.country')"></i>
                     {{ $countries[$venues[$event->venue_id]] }}
                 </div>
                 <div class="col-12 pb-2 action">
                     <form action="{{ route('events.destroy',$event->id) }}" method="POST">
 
-                        <a class="btn btn-info mr-2" href="{{ route('events.show',$event->id) }}">View</a>
-                        <a class="btn btn-primary" href="{{ route('events.edit',$event->id) }}">Edit</a>
+                        <a class="btn btn-info mr-2" href="{{ route('events.show',$event->id) }}">@lang('views.view')</a>
+                        <a class="btn btn-primary" href="{{ route('events.edit',$event->id) }}">@lang('views.edit')</a>
 
                         @csrf
                         @method('DELETE')
 
-                        <button type="submit" class="btn btn-danger float-right">Delete</button>
+                        <button type="submit" class="btn btn-danger float-right">@lang('views.delete')</button>
                     </form>
                 </div>
             </div>
