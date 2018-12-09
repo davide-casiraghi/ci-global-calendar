d7002c48202d664543d741488af48f254585e88e
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-09 18:50:30 +0100

    user factory done
    Former-commit-id: 19f974a1517869251972d1348c85f94d2a12a65a

--- blog/database/factories/UserFactory.php
+++ blog/database/factories/UserFactory.php
@@ -19,5 +19,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'group' => $faker->randomElement($array = array (2,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)), // 1 super admin, 2 admin
+        'country_id' => $faker->numberBetween($min = 1, $max = 253),
+        'description' => $faker->paragraph,
+        'accept_terms' => 1,
+        'activation_code' => str_random(10),
+        'status' => $faker->randomElement($array = array (1,1,1,1,1,1,1,0)), // 1 enabled, 0 disabled
     ];
 });
