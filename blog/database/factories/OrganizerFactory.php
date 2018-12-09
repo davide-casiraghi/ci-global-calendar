8b8140e92c009dc6396520600ac480187c19cf2b
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-09 19:26:40 +0100

    created factory for organizer
    Former-commit-id: 7aa07fee23e35552ed9e146cd648b60842d384ec

---
+++ blog/database/factories/OrganizerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Organizer::class, function (Faker $faker) {
+    
+    $organizer_name = $faker->name;
+    $slug = str_slug($organizer_name, '-').rand(10000, 100000);
+    $year_starting_practice = $faker->numberBetween($min = 1972, $max = 2018);
+    $year_starting_teach = $faker->numberBetween($min = $year_starting_practice, $max = 2018);
+    
+    return [
+        'name' => $organizer_name,
+        'website' => $faker->url,
+        'created_by' => '2',
+        'slug' => $slug,
+        'email' => $faker->unique()->safeEmail,
+        'description' => $faker->paragraph,
+        'phone' => $faker->e164PhoneNumber,
+    ];
+});
