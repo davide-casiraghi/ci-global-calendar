76b3ff4ce305a462ec6a186a084191bbe050a291
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-10 19:58:49 +0100

    added test for login
    Former-commit-id: 4792725b8ee1de6084e924fa3f6c854f88ee5ed2

---
+++ blog/tests/Browser/LoginTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use App\User;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->assertPathIs('/');
+        });
+    }
+}
