3ff70d169fdfb55187dbfd6a7e315f2e37a48703
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-10 14:20:03 +0100

    test adding a new teacher done
    Former-commit-id: f52dec0de913893d07ff3a6d2b3a899381d28b3a

--- blog/tests/Unit/TeacherTest.php
+++ blog/tests/Unit/TeacherTest.php
@@ -26,27 +26,22 @@ class TeacherTest extends TestCase
         
         // Post a data to create teacher
             $data = [
-                //'id' => $this->teachers->id,
                 'name' => $this->faker->name,
                 'bio' => $this->faker->paragraph,
                 'year_starting_practice' => "2000",
                 'year_starting_teach' => "2006",
                 'significant_teachers' => $this->faker->paragraph,
-                'profile_picture' => str_random(10).".jpg",
                 'website' => $this->faker->url,
                 'facebook' => "https://www.facebook.com/".$this->faker->word,
-                'created_by' => 1,
-                'slug' => $this->faker->slug,
                 'country_id' => $this->faker->numberBetween($min = 1, $max = 253),
             ];
             $response = $this->post('/teachers', $data);
-            //$response = $this->json('POST', '/teachers', $data);
-            dd($response);
+            
         // Assert in database
             $this->assertDatabaseHas('teachers',$data);
             
         // Status
-            $response->assertStatus(201);
+            $response->assertStatus(302); // I aspect redirect (301 or 302) because after store get redirected to teachers.index
     
     }
     
