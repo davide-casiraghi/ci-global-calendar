70be0aaccfa0125c95b36e699fc4dddc510cd1e9
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-11 23:02:18 +0100

    now we test the creation of a new teacher
    Former-commit-id: 4c68ab75c98f80033b2e2ee312374bd543a80cb8

--- blog/tests/Browser/TeachersTest.php
+++ blog/tests/Browser/TeachersTest.php
@@ -52,18 +52,22 @@ class TeachersTest extends DuskTestCase
       * @param  string  $name
       * @return void
       */
-     public function create_new_teacher(Browser $browser)
+     public function create_new_teacher()
      {
-                    $browser->type('name', 'Test Teacher')
-                    ->type('country', 5)
-                    ->type('bio', 'lorem ipsum dolet')
-                    ->type('year_starting_practice', '1999')
-                    ->type('year_starting_teach', '1995')
-                    ->type('significant_teachers', 'test teachers')
-                    ->type('facebook', 'http://www.facebook.com/test')
-                    ->type('website', 'http://www.test.it')
-                ->press('Submit')
-                ->assertSee('Teachers management');
+         $this->browse(function (Browser $browser) {
+             $browser->visit('/teachers')   //dusk don't like to visit page /teachers/create, so let's go there clicking
+                   ->clickLink('Add new teacher')
+                   ->type('name', 'Test Teacher')
+                   ->type('country', 5)
+                   ->type('bio', 'lorem ipsum dolet')
+                   ->type('year_starting_practice', '1999')
+                   ->type('year_starting_teach', '1995')
+                   ->type('significant_teachers', 'test teachers')
+                   ->type('facebook', 'http://www.facebook.com/test')
+                   ->type('website', 'http://www.test.it')
+               ->press('Submit')
+               ->assertSee('Teachers management');
+         });
      }
     
     
