399d8513e8cd264184f0b07a82d05c830548db02
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-13 07:35:17 +0100

    added CategoriesTableSeeder and EventCategoriesTableSeeder
    Former-commit-id: dd14219d6b1603129425464923ca44758a52161b

---
+++ blog/database/seeds/EventCategoriesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EventCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $event_categories = array(
+           array('id' => '1','name' => 'Regular Jam','slug' => ''),
+           array('id' => '2','name' => 'Class','slug' => ''),
+           array('id' => '3','name' => 'Workshop','slug' => ''),
+           array('id' => '6','name' => 'Festival','slug' => ''),
+           array('id' => '10','name' => 'Special Jam','slug' => ''),
+           array('id' => '11','name' => 'Underscore','slug' => ''),
+           array('id' => '12','name' => 'Teachers Meeting','slug' => ''),
+           array('id' => '13','name' => 'Performance','slug' => ''),
+           array('id' => '14','name' => 'Lecture / Conference / Film','slug' => ''),
+           array('id' => '15','name' => 'Lab','slug' => ''),
+           array('id' => '16','name' => 'Camp / Journey','slug' => ''),
+           array('id' => '17','name' => 'Other event','slug' => '')
+       );
+       
+       foreach($event_categories as $key => $event_category) {
+           DB::table('event_categories')->insert([
+               'id' => $event_category['id'],
+               'name' => $event_category['name'],
+           ]);
+       }
+    }
+}
