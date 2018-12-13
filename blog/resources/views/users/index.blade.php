6badc44eae417150385639d3c7377174f4df91cc
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-13 08:32:35 +0100

    refactored users filters
    Former-commit-id: 667b263ddfac61ac38e507752f006e9fae2266ed

--- blog/resources/views/users/index.blade.php
+++ blog/resources/views/users/index.blade.php
@@ -3,7 +3,7 @@
 @section('javascript-document-ready')
     {{--  Clear filters on click reset button --}}
         $("#resetButton").click(function(){
-            $("input#keywords").val("");
+            $("input[name=keywords]").val("");
             $("select[name=country_id] option").prop("selected", false).trigger('change');
             $('form.searchForm').submit();
         });
@@ -30,16 +30,20 @@
     {{-- Search form --}}
     <form class="row mt-3 searchForm" action="{{ route('users.index') }}" method="GET">
         @csrf
-        <div class="form-group col-12 col-sm-6 col-md-6 col-lg-6">
-            <input type="text" name="keywords" id="keywords" class="form-control" placeholder="@lang('views.search_by_user_name')" value="{{ $searchKeywords }}">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6">
+            @include('partials.forms.input', [
+                'name' => 'keywords',
+                'placeholder' => __('views.search_by_user_name'),
+                'value' => $searchKeywords
+            ])
         </div>
         <div class="col-12 col-sm-6 col-md-6 col-lg-3">
-            <select name="country_id" class="selectpicker" data-live-search="true">
-                <option value="">@lang('views.filter_by_country')</option>
-                @foreach ($countries as $value => $country)
-                    <option value="{{$value}}" {{ $searchCountry == $value ? 'selected' : '' }} >{{ $country }} </option>
-                @endforeach
-            </select>
+            @include('partials.forms.select', [
+                'name' => 'country_id',
+                'placeholder' => __('views.filter_by_country'),
+                'records' => $countries,
+                'seleted' => $searchCountry
+            ])
         </div>
         <div class="col-12 col-sm-12 col-md-12 col-lg-3 mt-3 mt-sm-2 mt-lg-0">
             <a id="resetButton" class="btn btn-info float-right ml-2" href="#">@lang('general.reset')</a>
