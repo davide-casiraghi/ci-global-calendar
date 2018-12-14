54a04cad28be5368b0770a93ac546f13450d632b
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-14 07:25:33 +0100

    changed name to database for tests

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'forunittest',
+            'database' => 'CIGC-local-test',
             'username' => 'root',
             'password' => 'iokiok',
             'unix_socket' => env('DB_SOCKET', ''),
