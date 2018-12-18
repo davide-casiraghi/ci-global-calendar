a45dd7488f7559977174c2e57572d31ce24e84d1
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-18 11:11:35 +0100

    now test also if user can search in HP

--- tests/Unit/HomepageTest.php
+++ tests/Unit/HomepageTest.php
@@ -9,15 +9,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class HomepageTest extends TestCase
 {
     use WithFaker;
+    
+    /***************************************************************************/
     /**
-     * A basic test example.
-     *
-     * @return void
-     */
+     * Test that logged user can see the homepage
+     */  
     public function test_guest_user_can_see_homepage()
     {
-        
-
         // Access to the page
             $response = $this->get('/')
                              ->assertStatus(200);
