54b3167ea290c6e2c0fef54ce052837dbe204fbc
Davide Casiraghi <davide.casiraghi@gmail.com>
2018-12-22 18:37:21 +0100

    Enabled mobile devices native menu for selectpicker menus

--- resources/views/partials/forms/select.blade.php
+++ resources/views/partials/forms/select.blade.php
@@ -1,3 +1,10 @@
+@section('javascript-document-ready')
+    @parent
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        $('.selectpicker').selectpicker('mobile');
+    }
+@stop
+
 <div class="form-group">
     @if(!empty($title))<label for="{{ $name }}">{{ $title }}</label>@endif
     <select name="{{ $name }}" class="selectpicker" data-live-search="true" title="{{$placeholder}}">
