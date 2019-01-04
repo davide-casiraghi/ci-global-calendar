49f5a3bb980563d51b6e11f2f140a65877f99245
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-04 21:24:02 +0100

    added google analitics

--- resources/js/utility/google_analytics.js
+++ resources/js/utility/google_analytics.js
@@ -6,5 +6,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-3675475-39', 'movementmeetslife.com');
+  ga('create', 'UA-3675475-42', 'ciglobalcalendar.net');
   ga('send', 'pageview');
