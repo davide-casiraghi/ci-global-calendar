e01a7403abe49a9a2e16647813d905915584482f
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-05 13:58:10 +0100

    fixed some w3c bugs after running validator

--- resources/views/menus/nav.blade.php
+++ resources/views/menus/nav.blade.php
@@ -4,7 +4,7 @@
 <nav class="navbar navbar-expand-lg navbar-light"> {{--navbar-dark bg-dark--}}
     <div class="container">
         {{--<a class="navbar-brand" href="#">Navbar</a>--}}
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-brand d-block d-md-none">
