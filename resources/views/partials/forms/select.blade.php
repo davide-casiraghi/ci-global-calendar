27ee90f508da15d717406da7926469da87bd6cd0
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-05 13:53:00 +0100

    fixed problem with labels id

--- resources/views/partials/forms/select.blade.php
+++ resources/views/partials/forms/select.blade.php
@@ -7,7 +7,7 @@
 
 <div class="form-group">
     @if(!empty($title))<label for="{{ $name }}">{{ $title }}</label>@endif
-    <select name="{{ $name }}" class="selectpicker" data-live-search="true" title="{{$placeholder}}">
+    <select name="{{ $name }}" id="{{ $name }}" class="selectpicker" data-live-search="true" title="{{$placeholder}}">
         @foreach ($records as $value => $record)
             <option value="{{$value}}" @if(!empty($seleted)) {{  $seleted == $value ? 'selected' : '' }}@endif>{{ $record }}</option>
         @endforeach
