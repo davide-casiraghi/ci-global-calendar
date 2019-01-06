fbddd547ddc7ca92ef9221ced8d657aeafc66d4c
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-06 11:43:57 +0100

    added some translations

---
+++ resources/lang/de/misuse.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'misuse' => 'Falscher Gebrauch',
+    'report_misuse' => 'Falschen Gebrauch melden',
+    'report_violation' => 'Bu bist dabei einen Verstoß gegen die Nutzungsbedingungen des Kalenders zu melden',
+    'reason' => 'Grund',
+    'select_one_option' => 'Eine Option auswählen',
+    'not_about_ci' => 'Gehört nicht zu Contact Improvisation',
+    'contains_wrong_info' => 'Beinhaltet falsche Informationen',
+    'not_translated_english' => 'Ist nicht auf Englisch übersetzt',
+    'other' => 'Anderer Grund (genuer beschreiben)',
+    'message' => 'Nachricht (optional)',
+    'include_all_details' => 'Schließe möglichst alle Details ein',
+];
---
+++ resources/lang/es/misuse.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'misuse' => 'Uso indebido',
+    'report_misuse' => 'Reportar uso indevido',
+    'report_violation' => 'Estás a punto de reportar una violación de los terminos de uso del calendario',
+    'reason' => 'Motivo',
+    'select_one_option' => 'Selecciona una opción',
+    'not_about_ci' => 'No se trata de Contact Improvisación',
+    'contains_wrong_info' => 'Contiene información errónea',
+    'not_translated_english' => 'No esta traducido al Inglés',
+    'other' => 'Otra (especificar en el mensaje)',
+    'message' => 'Mensaje (opcional)',
+    'include_all_details' => 'Incluye todos los detalles que puedas',
+];
