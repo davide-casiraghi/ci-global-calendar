aa142815e0256c52931283ecfc04f6e71352e279
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-07 21:08:08 +0100

    added global archive link

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -6,7 +6,7 @@
                 <div class="text text-white">© 2017, made with <i class="fas fa-heart"></i> by Round Robin Team</div>
             </div>
             <div class="col-12 col-sm-5 col-sm-push-5 text-center text-md-right" >
-                <a href="#" class="mr-2 text-white"><i class="fa fa-globe"></i> CI - Global Archive</a>
+                <a href="/posts/43" class="mr-2 text-white"><i class="fa fa-globe"></i> CI - Global Archive</a>
                 <a href="#" class="text-white"><i class="fa fa-heart"></i> @lang('menu.donate') </a>
                 {{--<p class="float-right"><a href="#">Back to top</a></p>--}}
             </div>
