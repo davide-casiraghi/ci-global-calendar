475d3d57f40286f110f423fed9fef114c5a27544
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-07 19:11:20 +0100

    adding dd to accordion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -129,7 +129,7 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Post $post){
-            dump("iii");
+            dump($post);
         // Accordion
             $accordionClass = new AccordionClass();
             $post->body = $accordionClass->getAccordion($post->body);
