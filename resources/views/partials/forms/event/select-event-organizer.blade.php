b67c3fc51fd08a9103f37337d0d615e780d72ba4
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-10 12:23:15 +0100

    clarified the event selection of teachers, organizers, venues

--- resources/views/partials/forms/event/select-event-organizer.blade.php
+++ resources/views/partials/forms/event/select-event-organizer.blade.php
@@ -26,7 +26,7 @@
             <input type="hidden" name="multiple_organizers" id="multiple_organizers" @if(!empty($multiple_organizers)) value="{{$multiple_organizers}}" @endif/>
         </div>
     </div>
-    <div class="col-12 col-sm-6 col-md-6">
+    <div class="col-12 col-sm-6 col-md-6 mt-0 mt-sm-2">
         <button type="button" data-toggle="modal" class="btn btn-primary mb-3 mb-sm-0 mt-sm-4" data-remote="{{ route('organizers.modal') }}" data-target=".modalFrame"><i class="fa fas fa-plus-circle "></i> @lang('views.add_new_organizer')</button>
     </div>
 </div>
--- resources/views/partials/forms/event/select-event-teacher.blade.php
+++ resources/views/partials/forms/event/select-event-teacher.blade.php
@@ -27,7 +27,7 @@
             <input type="hidden" name="multiple_teachers" id="multiple_teachers" @if(!empty($multiple_teachers))  value="{{$multiple_teachers}}" @endif/>
         </div>
     </div>
-    <div class="col-12 col-sm-6 col-md-6">
+    <div class="col-12 col-sm-6 col-md-6 mt-0 mt-sm-2">
         <button type="button" data-toggle="modal" class="btn btn-primary mb-3 mb-sm-0 mt-sm-4" data-remote="{{ route('teachers.modal') }}" data-target=".modalFrame"><i class="fa fas fa-plus-circle "></i> @lang('views.add_new_teacher')</button>
     </div>
 </div>
