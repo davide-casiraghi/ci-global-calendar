aedb2489a077efdf8a4328fba08d7dc3d3551f70
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-11 23:17:18 +0100

    simplified dates in HP

--- resources/views/partials/forms/event-search/input-date-start.blade.php
+++ resources/views/partials/forms/event-search/input-date-start.blade.php
@@ -17,10 +17,10 @@
 
 <div class="form-group">
     <div class="input-group input-append date datepicker_start_date" data-date-format="dd-mm-yyyy">
-        <div class="input-group-append">
+        {{--<div class="input-group-append">
           <div class="input-group-text">@lang('homepage-serach.start_on')<i class="far fa-calendar-alt ml-2"></i></div>
-        </div>
-        <input name="startDate" id="startDate" class="form-control" type="text" placeholder="Select date" @if(!empty($searchStartDate)) value="{{ $searchStartDate }}" @endif readonly="readonly" aria-describedby="startDate">
+      </div>--}}
+        <input name="startDate" id="startDate" class="form-control" type="text" placeholder="@lang('homepage-serach.start_on')" @if(!empty($searchStartDate)) value="{{ $searchStartDate }}" @endif readonly="readonly" aria-describedby="startDate">
 
     </div>
 </div>
