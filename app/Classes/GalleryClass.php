0974a1d978d2124e05732bed4e368d2f29435962
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-13 11:30:57 +0100

    refactoring gallery code

--- app/Classes/GalleryClass.php
+++ app/Classes/GalleryClass.php
@@ -4,6 +4,7 @@
     This plugin show a responsive gallery
     Example of strings that evoke the plugin:
     {# gallery src=[contact_improvisation/gallery_1] width=[400] height=[300] #}
+    {# gallery src=[1/background_homepage] width=[400] height=[300] #}
 */
 
 namespace App\Classes;
