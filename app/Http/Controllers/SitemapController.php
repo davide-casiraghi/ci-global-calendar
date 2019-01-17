4348691771926dad5eda6145e9eadc71fb7a42bc
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-17 09:16:45 +0100

    created route for sitemap controller

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -11,15 +11,33 @@ class SitemapController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\EventVenue  $eventVenue
      * @return \Illuminate\Http\Response
      */
-    public function show(){
-        $sitemap = "aa";
+    public function show(){        
+        $sitemap = $this->generateSitemapXML();
         return view('sitemap.show',['sitemap' => $sitemap]);
     }
     
-    
+    /***************************************************************************/
+    /**
+     * Generate the sitemap XML
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function generateSitemapXML(){
+        $ret = "";
+        
+        
+        $ret .= "<?xml version='1.0' encoding='UTF-8'?>";
+            $ret .= "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'> ";
+                $ret .= "<url>";
+                    $ret .= "<loc>http://www.example.com/foo.html</loc>";
+                    $ret .= "<lastmod>2018-06-04</lastmod>";
+                $ret .= "</url>";
+        $ret .= "</urlset>";
+        
+        return $ret;
+    }    
     
     
     
