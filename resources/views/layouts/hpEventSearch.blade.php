2710d41718cebdfe94734c53c4ce913119868ead
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-17 13:30:25 +0100

    now menus can be wrapped in container or not

--- resources/views/layouts/hpEventSearch.blade.php
+++ resources/views/layouts/hpEventSearch.blade.php
@@ -30,7 +30,11 @@
 
 <body class=""> {{-- Laravel use VUE as default - https://stackoverflow.com/questions/41411344/vue-warn-cannot-find-element-app#41411385 --}}
     {{-- {!! menu('main', 'nav') !!} --}}
-    @include('menus.nav', ['items' => $MyNavBar->roots()])
+    @include('menus.nav', [
+        'items' => $MyNavBar->roots(),
+        'container' => true,
+        'paddingX' => '',
+    ])
 
     <div id="app" class="beforeContent">
         @yield('beforeContent')
