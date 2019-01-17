b294e704b7f783f3f8ba0f2b395fa2d0eff43990
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-17 10:56:21 +0100

    populating sitemap controller

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class SitemapController extends Controller
 {
     
+    
+    public function index()
+    {
+      $post = Post::active()->orderBy('updated_at', 'desc')->first();
+      $podcast = Podcast::active()->orderBy('updated_at', 'desc')->first();
+
+      return response()->view('sitemap.index', [
+          'post' => $post,
+          'podcast' => $podcast,
+      ])->header('Content-Type', 'text/xml');
+    }
+    
     /***************************************************************************/
     /**
      * Display the specified resource.
