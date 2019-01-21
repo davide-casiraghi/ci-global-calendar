7c8a90051df10ea7414788eaba2c1f4b3dfbb1c7
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-21 16:54:36 +0100

    footer menu is now the same color of header menu and show darker hover

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -11,13 +11,16 @@
 {{--<hr class="mt-5">--}}
 <footer class="{{$paddingX}}" style="background-color: {{$backgroundColor}}">
     @if($container)<div class="container">@endif
-        <nav class="row py-2">
+        <nav class="row">
             <div class="col-12 col-sm-6 col-sm-pull-6 text-center text-md-left mb-2 mb-sm-0">
-                <div class="text text-white">© 2019, made with <i class="fas fa-heart"></i> by Round Robin Team</div>
+                <div class="text text-white pt-2 pb-0 pb-md-2">© 2019, made with <i class="fas fa-heart"></i> by Round Robin Team</div>
             </div>
             <div class="col-12 col-sm-5 col-sm-push-5 text-center text-md-right" >
-                <a href="/post/contact-improvisation-global-archive-ciga" class="mr-2 text-white"><i class="fa fa-globe"></i> CI - Global Archive</a>
-                <a href="/post/donate" class="text-white"><i class="fa fa-heart"></i> @lang('menu.donate') </a>
+                <div class="footerMenu">
+                    <a href="/post/contact-improvisation-global-archive-ciga" class="text-white"><i class="fa fa-globe"></i> CI - Global Archive</a>
+                    <a href="/post/donate" class="text-white"><i class="fa fa-heart"></i> @lang('menu.donate') </a>
+                </div>
+                
                 {{--<p class="float-right"><a href="#">Back to top</a></p>--}}
             </div>
             <div class="col-12 col-sm-1 col-sm-push-1 text-center text-md-right d-none d-md-block">
