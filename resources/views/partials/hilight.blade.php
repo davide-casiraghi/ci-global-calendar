6ece4031a3b2db5dcaee7ac63b950e8ff87c6978
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-01-30 10:27:33 +0100

    start creating the hilight partial

---
+++ resources/views/partials/hilight.blade.php
@@ -0,0 +1,9 @@
+{{--
+
+    HILIGHT
+    Show a box with a text that can be closed and now shown until the cookie expire.
+    
+    PARAMETERS:
+        - $events: array - the list of events
+        - $iframeLinkBlank: boolean - if true clicking on an event bring the user out of the IFRAME where the page was loaded (used for regional websites)
+--}}
