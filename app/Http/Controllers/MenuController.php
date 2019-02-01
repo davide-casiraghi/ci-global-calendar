42c2eff6d614c3358aec71b66b7d23b6ee83037d
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-01 19:15:21 +0100

    cleaned menu functions

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -26,11 +26,7 @@ class MenuController extends Controller
      */
     public function create(){
 
-        $categories = Category::pluck('name', 'id');
-        //dump($categories);
-
-        //return view('posts.create');
-        return view('menus.create')->with('categories', $categories);
+        
     }
     
     
@@ -43,49 +39,31 @@ class MenuController extends Controller
      */
     public function store(Request $request){
         
-        // Validate form datas
-            $validator = Validator::make($request->all(), [
-                'title' => 'required',
-                'body' => 'required',
-                'category_id' => 'required',
-            ]);
-            if ($validator->fails()) {
-                return back()->withErrors($validator)->withInput();
-            }
         
-        $post = new Post();
-        
-        // Set the default language to edit the post for the admin to English (to avoid bug with null titles)
-            App::setLocale('en');
-            
-        $this->saveOnDb($request, $post);    
-
-        return redirect()->route('menus.index')
-                        ->with('success',__('messages.article_added_successfully'));
     }
 
     /***************************************************************************/
     /**
      * Display the specified resource.
      *
-     * @param  \App\Country  $country
+     * @param  \App\Menu  $country
      * @return \Illuminate\Http\Response
      */
-    public function show(Country $country){
-        return view('menus.show',compact('country'));
+    public function show(Menu $menu){
+        return view('menus.show',compact('menu'));
     }
     
     /***************************************************************************/
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Country  $country
+     * @param  \App\Menu  $country
      * @return \Illuminate\Http\Response
      */
-    public function edit(Country $country){
-        $continents = Continent::pluck('name', 'id');
+    public function edit(Menu $menu){
+        
 
-        return view('countries.edit',compact('country'))->with('continents',$continents);
+        return view('menus.edit',compact('country'))->with('continents',$continents);
     }
 
     /***************************************************************************/
@@ -93,10 +71,10 @@ class MenuController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Post  $post
+     * @param  \App\Menu  $menu
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post){
+    public function update(Request $request, Menu $menu){
 
         request()->validate([
             'title' => 'required',
@@ -104,27 +82,23 @@ class MenuController extends Controller
             'category_id' => 'required',
         ]);
         
-        
-        // Set the default language to edit the post for the admin to English (to avoid bug with null titles)
-            App::setLocale('en');
-        
         $this->saveOnDb($request, $post);
 
-        return redirect()->route('posts.index')
-                        ->with('success',__('messages.article_updated_successfully'));
+        return redirect()->route('menus.index')
+                        ->with('success',__('messages.menu_updated_successfully'));
     }
     
     /***************************************************************************/
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Country  $country
+     * @param  \App\Menu  $menu
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Country $country){
-        $country->delete();
-        return redirect()->route('countries.index')
-                        ->with('success',__('messages.country_deleted_successfully'));
+    public function destroy(Menu $menu){
+        $menu->delete();
+        return redirect()->route('menus.index')
+                        ->with('success',__('messages.menu_deleted_successfully'));
     }
 
     /***************************************************************************/
