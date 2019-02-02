3f1d5371f66faec8e729671cec61644389c18b48
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-02 07:39:54 +0100

    added route

--- database/seeds/MenuItemsTableSeeder.php
+++ database/seeds/MenuItemsTableSeeder.php
@@ -12,10 +12,46 @@ class MenuItemsTableSeeder extends Seeder
     public function run()
     {
         $menuItems = array(
-           array('id' => '1','name' => 'Home','compact_name' => 'home','lang_string' => 'menu.home','parent_item' => '','url' => '/','font_awesome_class' => 'fa fa-home'),
-           array('id' => '2','name' => 'About','compact_name' => 'about','lang_string' => 'menu.about','parent_item' => '','url' => '/post/about','font_awesome_class' => 'fa fa-info-circle'),
-           array('id' => '3','name' => 'Terms of use','compact_name' => 'terms_of_use','lang_string' => 'menu.terms_of_use','parent_item' => '','url' => '/post/terms-of-use','font_awesome_class' => 'far fa-file-alt'),
-           array('id' => '4','name' => 'Teachers directory','compact_name' => 'teachers_directory','lang_string' => 'menu.teachers_directory','parent_item' => '3','url' => '/post/terms-of-use','font_awesome_class' => 'far fa-file-alt'),
+           array('id' => '1',
+                    'name' => 'Home',
+                    'compact_name' => 'home',
+                    'type' => '0',
+                    'lang_string' => 'menu.home',
+                    'parent_item' => '',
+                    'url' => '/',
+                    'route' => '',
+                    'font_awesome_class' => 'fa fa-home'
+                ),
+           array('id' => '2',
+                   'name' => 'About',
+                   'compact_name' => 'about',
+                   'type' => '0',
+                   'lang_string' => 'menu.about',
+                   'parent_item' => '',
+                   'url' => '/post/about',
+                   'route' => '',
+                   'font_awesome_class' => 'fa fa-info-circle'
+           ),
+           array('id' => '3',
+                   'name' => 'Terms of use',
+                   'compact_name' => 'terms_of_use',
+                   'type' => '0',
+                   'lang_string' => 'menu.terms_of_use',
+                   'parent_item' => '',
+                   'url' => '/post/terms-of-use',
+                   'route' => '',
+                   'font_awesome_class' => 'far fa-file-alt'
+           ),
+           array('id' => '4',
+                   'name' => 'Teachers directory',
+                   'compact_name' => 'teachers_directory',
+                   'type' => '1',
+                   'lang_string' => 'menu.teachers_directory',
+                   'parent_item' => '3',
+                   'url' => '',
+                   'route' => '',
+                   'font_awesome_class' => 'far fa-file-alt'
+           ),
        );
        
        foreach($menuItems as $key => $menuItem) {
@@ -23,8 +59,10 @@ class MenuItemsTableSeeder extends Seeder
                'id' => $menuItem['id'],
                'name' => $menuItem['name'],
                'compact_name' => $menuItem['compact_name'],
+               'type' => $menuItem['type'],
                'parent_item' => $menuItem['parent_item'],
                'url' => $menuItem['url'],
+               'route' => $menuItem['route'],
                'font_awesome_class' => $menuItem['font_awesome_class'],
            ]);
        }
