c92e8f0a6d0cb4b403cc5d347b1c4c59082d488c
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-02 07:51:01 +0100

    added menu id

--- database/seeds/MenuItemsTableSeeder.php
+++ database/seeds/MenuItemsTableSeeder.php
@@ -14,6 +14,8 @@ class MenuItemsTableSeeder extends Seeder
         $menuItems = array(
            array('id' => '1',
                     'name' => 'Home',
+                    'menu_id' => '1',
+                    'order' => '0',
                     'compact_name' => 'home',
                     'type' => '0',
                     'lang_string' => 'menu.home',
@@ -22,56 +24,7 @@ class MenuItemsTableSeeder extends Seeder
                     'route' => '',
                     'font_awesome_class' => 'fa fa-home'
                 ),
-           array('id' => '2',
-                   'name' => 'About',
-                   'compact_name' => 'about',
-                   'type' => '0',
-                   'lang_string' => 'menu.about',
-                   'parent_item' => '',
-                   'url' => '/post/about',
-                   'route' => '',
-                   'font_awesome_class' => 'fa fa-info-circle'
-           ),
-           array('id' => '3',
-                   'name' => 'Terms of use',
-                   'compact_name' => 'terms_of_use',
-                   'type' => '0',
-                   'lang_string' => 'menu.terms_of_use',
-                   'parent_item' => '',
-                   'url' => '/post/terms-of-use',
-                   'route' => '',
-                   'font_awesome_class' => 'far fa-file-alt'
-           ),
-           array('id' => '4',
-                   'name' => 'Teachers directory',
-                   'compact_name' => 'teachers_directory',
-                   'type' => '1',
-                   'lang_string' => 'menu.teachers_directory',
-                   'parent_item' => '3',
-                   'url' => '',
-                   'route' => 'teachers.directory',
-                   'font_awesome_class' => 'far fa-file-alt'
-           ),
-           array('id' => '5',
-                   'name' => 'Get Involved',
-                   'compact_name' => 'get_involved',
-                   'type' => '0',
-                   'lang_string' => 'menu.teachers_directory',
-                   'parent_item' => '3',
-                   'url' => '',
-                   'route' => 'teachers.directory',
-                   'font_awesome_class' => 'far fa-file-alt'
-           ),
-           array('id' => '6',
-                   'name' => 'Help',
-                   'compact_name' => 'help',
-                   'type' => '0',
-                   'lang_string' => 'menu.teachers_directory',
-                   'parent_item' => '3',
-                   'url' => '',
-                   'route' => 'teachers.directory',
-                   'font_awesome_class' => 'far fa-file-alt'
-           ),
+           
            
        );
        
