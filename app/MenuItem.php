ac83ff7eb1649d4ef55ef73e72c8ccaadf8eedba
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-02 10:01:02 +0100

    upated menu item model

--- app/MenuItem.php
+++ app/MenuItem.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
     protected $fillable = [
-        'name', 'compact_name', 'parent_item', 'link', 'font_awesome_class'
+        'name', 'compact_name', 'parent_item_id', 'url', 'font_awesome_class', 'lang_string','route','type','menu_id','order'
     ];
 }
