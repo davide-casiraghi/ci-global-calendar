66930f667e5367752c6c79f267a2af9edd9598c5
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-02 05:32:38 +0100

    creating menu seeders

---
+++ database/seeds/MenuItemsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MenuItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
