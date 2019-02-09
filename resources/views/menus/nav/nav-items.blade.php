e6e7559ff26509d5a49b465847bd93174d101164
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-09 14:20:54 +0100

    added language string to menu items in the frontend

--- resources/views/menus/nav/nav-items.blade.php
+++ resources/views/menus/nav/nav-items.blade.php
@@ -28,59 +28,38 @@
     
     
     @if($item->authorized())
-        @if(!empty($item->parent_item_id))
-           {{-- Element of submenu --}}
-           <li @if(!empty($item->children)) class="dropdown" @endif>
-        @else
-           {{-- Element of main menu --}}
-           <li @if(!empty($item->children)) class="nav-item dropdown" @else class="nav-item" @endif>
-        @endif
+        {{-- Get item name --}}
+            @if ($item->lang_string)
+                @php ($itemName = __($item->lang_string))
+            @else
+                @php ($itemName = $item->name)
+            @endif
+            
+        {{-- Render item --}}
+            @if(!empty($item->parent_item_id))
+               {{-- Element of submenu --}}
+               <li @if(!empty($item->children)) class="dropdown" @endif>
+            @else
+               {{-- Element of main menu --}}
+               <li @if(!empty($item->children)) class="nav-item dropdown" @else class="nav-item" @endif>
+            @endif
 
-        @if(!empty($item->parent_item_id))
-           <a @if(!empty($item->children)) class="dropdown-item has-submenu" @else class="dropdown-item" @endif href="{!! $url !!}" @if(!empty($onclick)) onclick="{{$onclick}}" @endif)>
-               @if(!empty($item->font_awesome_class))<i class="{{$item->font_awesome_class}}"></i>@endif @if(empty($item->hide_name)){{$item->name}}@endif
-           </a>
-        @else
-           <a @if(!empty($item->children)) class="nav-link has-submenu" @else class="nav-link" @endif href="{!! $url !!}">
-               @if(!empty($item->font_awesome_class))<i class="{{$item->font_awesome_class}}"></i>@endif @if(empty($item->hide_name)){{$item->name}}@endif
-           </a>
-        @endif
+            @if(!empty($item->parent_item_id))
+               <a @if(!empty($item->children)) class="dropdown-item has-submenu" @else class="dropdown-item" @endif href="{!! $url !!}" @if(!empty($onclick)) onclick="{{$onclick}}" @endif)>
+                   @if(!empty($item->font_awesome_class))<i class="{{$item->font_awesome_class}}"></i>@endif @if(empty($item->hide_name)){{$itemName}}@endif
+               </a>
+            @else
+               <a @if(!empty($item->children)) class="nav-link has-submenu" @else class="nav-link" @endif href="{!! $url !!}">
+                   @if(!empty($item->font_awesome_class))<i class="{{$item->font_awesome_class}}"></i>@endif @if(empty($item->hide_name)){{$itemName}}@endif
+               </a>
+            @endif
 
-
-         @if(!empty($item->children))
-           <ul class="dropdown-menu">
-                 @include('menus.nav.nav-items', ['items' => $item->children])
-           </ul>
-         @endif
+        {{-- Sub items --}}
+             @if(!empty($item->children))
+               <ul class="dropdown-menu">
+                     @include('menus.nav.nav-items', ['items' => $item->children])
+               </ul>
+             @endif
      @endif
     
-    
-    
-    {{--
-    <li class="nav-item @if(!empty($item->children))dropdown @endif"> 
-        <a class="nav-link @if(!empty($item->children))has-submenu @endif" href="{{$item->url}}">
-            @if(!empty($item->font_awesome_class))<i class="{{$item->font_awesome_class}}"></i>@endif {{$item->name}}
-            @if(!empty($item->children))<span class="sub-arrow"></span> @endif
-        </a>
-        @if(!empty($item->children))
-            <ul class="dropdown-menu">
-             @include('menus.nav.nav-items', ['items' => $item->children])
-            </ul>
-        @endif
-        
-    </li>--}}
 @endforeach
-
-{{-- @if (count($menu_item->children ) > 0)
-    <li class='nav-item dropdown'>
-@else
-   <li class='nav-item'>
-@endif --}}
-
-
-{{--<li class="nav-item">
-                        <a class="nav-link" href="/"><i class="fa fa-home"></i> Home </a>
-        
-
-      
-    </li>--}}
