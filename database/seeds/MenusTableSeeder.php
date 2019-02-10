366fe8d5ed473a4109a14984079dfbe613057a7d
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-10 16:29:59 +0100

    updated seeder for menus

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -12,14 +12,16 @@ class MenusTableSeeder extends Seeder
     public function run()
     {
         $menus = array(
-           array('id' => '1','name' => 'Admin menu'),
-           array('id' => '2','name' => 'Main menu'),
+           array('id' => '1','name' => 'Main menu - Left', 'position'=> '1'),
+           array('id' => '4','name' => 'Main menu - Right', 'position'=> '2'),
+           array('id' => '3','name' => 'Footer menu', 'position'=> '3'),
        );
        
        foreach($menus as $key => $menu) {
            DB::table('menus')->insert([
                'id' => $menu['id'],
                'name' => $menu['name'],
+               'position' => $menu['position'],
            ]);
        }
     }
