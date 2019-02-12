13d8715ca50eccb1e7326d256223b130c1686f5d
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-12 15:25:05 +0100

    now create translation works for menu items

--- resources/views/menuItemTranslations/create.blade.php
+++ resources/views/menuItemTranslations/create.blade.php
@@ -1,1 +1,49 @@
-create
+@extends('menuItemTranslations.layout')
+
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <h2>@lang('views.add_new_translation')</h2>
+        </div>
+        <div class="col-12 col-sm-6 text-right">
+            <span class="badge badge-secondary">{{$selectedLocaleName}}</span>
+        </div>
+    </div>
+
+    @include('partials.forms.error-management', [
+          'style' => 'alert-danger',
+    ])
+
+    
+    <form action="{{ route('menuItemTranslations.store') }}" method="POST">
+        @csrf
+
+            @include('partials.forms.input-hidden', [
+                  'name' => 'menu_item_id',
+                  'value' => $menuItemId,
+            ])
+            @include('partials.forms.input-hidden', [
+                  'name' => 'language_code',
+                  'value' => $languageCode
+            ])
+
+         <div class="row">
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'Name',
+                    'name' => 'name',
+                    'placeholder' => 'Menu item name',
+                    'value' => old('name')
+                ])
+            </div>
+        </div>
+
+        @include('partials.forms.buttons-back-submit', [
+            'route' => 'menuItems.index'  
+        ])
+
+    </form>
+
+@endsection
--- resources/views/menuItemTranslations/layout.blade.php
+++ resources/views/menuItemTranslations/layout.blade.php
@@ -1,1 +1,15 @@
-layout
+@extends('layouts.app')
+
+@section('title',  "Menu item translation" )
+
+@section('content')
+
+    <div class="container">
+
+		<div class="container">
+		    @yield('content')
+		</div>
+
+    </div>
+
+@endsection
