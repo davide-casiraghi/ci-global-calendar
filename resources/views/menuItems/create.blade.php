40b80a146d04db988a66969599479b7ef6b08651
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-13 11:46:48 +0100

    menu item create now shows english badge

--- resources/views/menuItems/create.blade.php
+++ resources/views/menuItems/create.blade.php
@@ -33,12 +33,13 @@
      
 @stop
 
-@section('content')
+@section('content')    
     <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>@lang('views.add_new_menu_item')</h2>
-            </div>
+        <div class="col-12 col-sm-6">
+            <h2>@lang('views.add_new_menu_item')</h2>
+        </div>
+        <div class="col-12 col-sm-6 text-right">
+            <span class="badge badge-secondary">English</span>
         </div>
     </div>
 
