a0531b3372fd144340b7aff88b7bc6c45b52aced
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-14 13:16:54 +0100

    created CategoryTranslation model and controller

---
+++ app/Http/Controllers/CategoryTranslationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CategoryTranslation;
+use Illuminate\Http\Request;
+
+class CategoryTranslationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CategoryTranslation  $categoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CategoryTranslation $categoryTranslation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CategoryTranslation  $categoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CategoryTranslation $categoryTranslation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CategoryTranslation  $categoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CategoryTranslation $categoryTranslation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CategoryTranslation  $categoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CategoryTranslation $categoryTranslation)
+    {
+        //
+    }
+}
