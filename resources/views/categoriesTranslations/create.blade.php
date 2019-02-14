fbf8a3e8e2c26aa9e8581be713a5ee54f85381e0
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-14 13:47:23 +0100

    created create view

---
+++ resources/views/categoriesTranslations/create.blade.php
@@ -0,0 +1,56 @@
+@extends('categoriesTranslations.layout')
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <h2>@lang('views.add_new_translation')</h2>
+        </div>
+        <div class="col-12 col-sm-6 text-right">
+            <span class="badge badge-secondary">{{$selectedLocaleName}}</span>
+        </div>
+    </div>
+
+    @include('partials.forms.error-management', [
+          'style' => 'alert-danger',
+    ])
+
+    
+    <form action="{{ route('categoryTranslations.store') }}" method="POST">
+        @csrf
+
+            @include('partials.forms.input-hidden', [
+                  'name' => 'category_id',
+                  'value' => $categoryId,
+            ])
+            @include('partials.forms.input-hidden', [
+                  'name' => 'language_code',
+                  'value' => $languageCode
+            ])
+
+         <div class="row">
+            <div class="col-12">
+                @include('partials.forms.input', [
+                    'title' => 'Name',
+                    'name' => 'name',
+                    'placeholder' => 'Category name',
+                    'value' => old('name')
+                ])
+            </div>
+            
+            <div class="col-12">
+                @include('partials.forms.textarea', [
+                      'title' => __('general.description'),
+                      'name' => 'description',
+                      'placeholder' => 'Description'
+                ])
+            </div>
+        </div>
+
+        @include('partials.forms.buttons-back-submit', [
+            'route' => 'categories.index'  
+        ])
+
+    </form>
+
+@endsection
