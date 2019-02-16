79d9a4bd07948d279def7df58744715947b271a8
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-16 18:15:01 +0100

    created EventCategoryTranslation controller and model!

---
+++ app/Http/Controllers/EventCategoryTranslationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EventCategoryTranslation;
+use Illuminate\Http\Request;
+
+class EventCategoryTranslationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EventCategoryTranslation $eventCategoryTranslation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EventCategoryTranslation $eventCategoryTranslation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EventCategoryTranslation $eventCategoryTranslation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EventCategoryTranslation $eventCategoryTranslation)
+    {
+        //
+    }
+}
