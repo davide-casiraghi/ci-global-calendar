d17dcb052a81a57d035286e9900fe4be44175330
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-16 18:50:08 +0100

    added translations to category event index

--- resources/views/eventCategories/index.blade.php
+++ resources/views/eventCategories/index.blade.php
@@ -24,9 +24,22 @@
     <div class="venuesList my-4">
         @foreach ($eventCategories as $eventCategory)
             <div class="row p-1 {{ $loop->index % 2 ? 'bg-light': 'bg-white' }}">
-                <div class="col-12 col-md-6 col-lg-8 py-3 title">
-                    <a href="{{ route('eventCategories.edit',$eventCategory->id) }}">{{ $eventCategory->name }}</a>
-                </div>
+                
+                {{-- Title --}}
+                    <div class="col-5 py-2 title">
+                        <a href="{{ route('eventCategories.edit',$eventCategory->id) }}">{{ $eventCategory->translate('en')->name }}</a>
+                    </div>
+                    
+                {{-- Translations --}}
+                    <div class="col-5 text-right translation mt-1">
+                        @foreach ($countriesAvailableForTranslations as $key => $countryAvTrans)
+                            @if($eventCategory->hasTranslation($key))
+                                <a href="/eventCategoryTranslations/{{ $eventCategory->id }}/{{ $key }}/edit" class="bg-success text-white px-2 py-1 mb-1 mb-lg-0 d-inline-block rounded">{{$key}}</a>
+                            @else
+                                <a href="/eventCategoryTranslations/{{ $eventCategory->id }}/{{ $key }}/create" class="bg-secondary text-white px-2 py-1 mb-1 mb-lg-0 d-inline-block rounded">{{$key}}</a>
+                            @endif
+                        @endforeach
+                    </div>
                 
                 <div class="col-12 pb-2 action">
                     <form action="{{ route('eventCategories.destroy',$eventCategory->id) }}" method="POST">
