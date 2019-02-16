ba082c7c8d8ee43c2cca5dd0d2a9b63d74a6e265
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-16 18:40:40 +0100

    populated eventCategoryTranslation controller

--- app/Http/Controllers/EventCategoryTranslationController.php
+++ app/Http/Controllers/EventCategoryTranslationController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\EventCategory;
 use App\EventCategoryTranslation;
+
 use Illuminate\Http\Request;
+
+use Validator;
 
 class EventCategoryTranslationController extends Controller
 {
+    /* Restrict the access to this resource just to logged in users */
+    public function __construct(){
+        $this->middleware('admin');
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -22,9 +31,13 @@ class EventCategoryTranslationController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create($eventCategoryId, $languageCode){
+        $selectedLocaleName = $this->getSelectedLocaleName($languageCode);
+        
+        return view('eventCategoryTranslations.create')
+                ->with('eventCategoryId',$eventCategoryId)
+                ->with('languageCode',$languageCode)
+                ->with('selectedLocaleName',$selectedLocaleName);
     }
 
     /**
@@ -33,9 +46,26 @@ class EventCategoryTranslationController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request){
+        // Validate form datas
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return back()->withErrors($validator)->withInput();
+            }
+
+        $eventCategoryTranslation = new EventCategoryTranslation();
+        $eventCategoryTranslation->event_category_id = $request->get('event_category_id');
+        $eventCategoryTranslation->locale = $request->get('language_code');
+        
+        $eventCategoryTranslation->name = $request->get('name');
+        $eventCategoryTranslation->slug = str_slug($eventCategoryTranslation->name, '-');
+
+        $eventCategoryTranslation->save();
+        
+        return redirect()->route('eventCategories.index')
+                        ->with('success','Translation created successfully.');
     }
 
     /**
@@ -44,8 +74,7 @@ class EventCategoryTranslationController extends Controller
      * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
      * @return \Illuminate\Http\Response
      */
-    public function show(EventCategoryTranslation $eventCategoryTranslation)
-    {
+    public function show(EventCategoryTranslation $eventCategoryTranslation){
         //
     }
 
@@ -55,9 +84,17 @@ class EventCategoryTranslationController extends Controller
      * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
      * @return \Illuminate\Http\Response
      */
-    public function edit(EventCategoryTranslation $eventCategoryTranslation)
-    {
-        //
+    public function edit($eventCategoryId, $languageCode){
+        $eventCategoryTranslation = EventCategoryTranslation::where('event_category_id', $categoryId)
+                        ->where('locale', $languageCode)
+                        ->first();
+                        
+        $selectedLocaleName = $this->getSelectedLocaleName($languageCode);
+        
+        return view('eventCategoryTranslations.edit',compact('eventCategoryTranslation'))
+                    ->with('eventCategoryId',$eventCategoryId)
+                    ->with('languageCode',$languageCode)
+                    ->with('selectedLocaleName',$selectedLocaleName);
     }
 
     /**
@@ -67,9 +104,20 @@ class EventCategoryTranslationController extends Controller
      * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, EventCategoryTranslation $eventCategoryTranslation)
-    {
-        //
+    public function update(Request $request){
+        request()->validate([
+            'name' => 'required',
+        ]);
+
+        $categoryTranslation = EventCategoryTranslation::where ('id', $request->get('event_category_translation_id'));
+
+        $event_category_t['name'] = $request->get('name');
+        $event_category_t['slug'] = str_slug($request->get('name'), '-');
+
+        $eventCategoryTranslation->update($event_category_t);
+
+        return redirect()->route('eventCategories.index')
+                        ->with('success','Translation updated successfully');
     }
 
     /**
@@ -78,8 +126,7 @@ class EventCategoryTranslationController extends Controller
      * @param  \App\EventCategoryTranslation  $eventCategoryTranslation
      * @return \Illuminate\Http\Response
      */
-    public function destroy(EventCategoryTranslation $eventCategoryTranslation)
-    {
+    public function destroy(EventCategoryTranslation $eventCategoryTranslation){
         //
     }
 }
