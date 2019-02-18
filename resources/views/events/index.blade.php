47350634d2e6972822156f12698c043323b3e1bd
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-18 14:30:16 +0100

    fixing filters

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -12,61 +12,68 @@
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <h2>@lang('views.events_management')</h2>
+    <div class="container max-w-md">
+        <div class="row">
+            <div class="col-12 col-sm-7">
+                <h3>@lang('views.events_management')</h3>
+            </div>
+            <div class="col-12 col-sm-5 mt-4 mt-sm-0 text-right px-0">
+                <a class="btn btn-success create-new" href="{{ route('events.create') }}"><i class="fa fas fa-plus-circle"></i> @lang('views.create_new_event')</a>
+            </div>
         </div>
-        <div class="col-12 mt-4 mt-sm-0 text-right">
-            <a class="btn btn-success create-new" href="{{ route('events.create') }}"><i class="fa fas fa-plus-circle"></i> @lang('views.create_new_event')</a>
-        </div>
-    </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success mt-4">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success mt-4">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
 
-    {{-- Search form --}}
-    <form class="row searchForm mt-3" action="{{ route('events.index') }}" method="GET">
-        @csrf
-        <div class="col-12 col-sm-12 col-md-4 col-lg-3 mb-2">
-            @include('partials.forms.input', [
-                'name' => 'keywords',
-                'placeholder' => __('views.search_by_event_name'),
-                'value' => $searchKeywords
-            ])
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
-            @include('partials.forms.select', [
-                'name' => 'category_id',
-                'placeholder' => __('views.filter_by_category'),
-                'records' => $eventCategories,
-                'seleted' => $searchCategory,
-                'liveSearch' => 'true',
-                'mobileNativeMenu' => false,
-            ])
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            @include('partials.forms.select', [
-                'name' => 'country_id',
-                'placeholder' => __('views.filter_by_country'),
-                'records' => $countries,
-                'seleted' => $searchCountry,
-                'liveSearch' => 'true',
-                'mobileNativeMenu' => false,
-            ])
-        </div>
-        <div class="col-12 col-lg-3 mt-3 mt-sm-2 mt-lg-0">
-            <input type="submit" value="@lang('general.search')" class="btn btn-primary float-right ml-2">
-            <a id="resetButton" class="btn btn-outline-primary float-right" href="#">@lang('general.reset')</a>
-        </div>
-    </form>
-    
-    {{-- List of events --}}
-    <div class="eventList my-4">
-        @foreach ($events as $event)
-            <div class="container max-w-md">
+        {{-- Search form --}}
+        <form class="searchForm mt-3" action="{{ route('events.index') }}" method="GET">
+            @csrf
+            <div class="row">
+                <div class="col-12 col-sm-8 px-0">
+                    @include('partials.forms.input', [
+                        'name' => 'keywords',
+                        'placeholder' => __('views.search_by_event_name'),
+                        'value' => $searchKeywords
+                    ])
+                </div>
+                <div class="col-12 col-sm-4 px-0">
+                    <input type="submit" value="@lang('general.search')" class="btn btn-primary float-right ml-2">
+                    <a id="resetButton" class="btn btn-outline-primary float-right" href="#">@lang('general.reset')</a>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-12 col-sm-6 px-0 pr-2">
+                    @include('partials.forms.select', [
+                        'name' => 'category_id',
+                        'placeholder' => __('views.filter_by_category'),
+                        'records' => $eventCategories,
+                        'seleted' => $searchCategory,
+                        'liveSearch' => 'true',
+                        'mobileNativeMenu' => false,
+                    ])
+                </div>
+                <div class="col-12 col-sm-6 px-0">
+                    @include('partials.forms.select', [
+                        'name' => 'country_id',
+                        'placeholder' => __('views.filter_by_country'),
+                        'records' => $countries,
+                        'seleted' => $searchCountry,
+                        'liveSearch' => 'true',
+                        'mobileNativeMenu' => false,
+                    ])
+                </div>    
+            </div>
+            
+            
+        </form>
+        
+        {{-- List of events --}}
+        <div class="eventList my-4">
+            @foreach ($events as $event)
                 <div class="row bg-white shadow-1 rounded mb-3">
                     
                     <div class="d-none d-sm-block col-sm-4 p-0">
@@ -130,12 +137,11 @@
                     
                     
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
+        
+
+        {!! $events->links() !!}
     </div>
-
-
-    {!! $events->links() !!}
-
 
 @endsection
