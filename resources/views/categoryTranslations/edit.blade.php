9ee02278deb103d823ea1de6e014b9e0434f65a4
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-19 11:34:01 +0100

    restyled create and edit translations

--- resources/views/categoryTranslations/edit.blade.php
+++ resources/views/categoryTranslations/edit.blade.php
@@ -1,73 +1,74 @@
 @extends('categoryTranslations.layout')
 
 @section('content')
-    
-    <div class="row">
-        <div class="col-12 col-sm-6">
-            <h2>@lang('views.edit_translation')</h2>
-        </div>
-        <div class="col-12 col-sm-6 text-right">
-            <span class="badge badge-secondary">{{$selectedLocaleName}}</span>
-        </div>
-    </div>
-
-    @include('partials.forms.error-management', [
-          'style' => 'alert-danger',
-    ])
-
-    <form action="{{ route('categoryTranslations.update') }}" method="POST">
-        @csrf
-        @method('PUT')
-
-            @include('partials.forms.input-hidden', [
-                  'name' => 'category_translation_id',
-                  'value' => $categoryTranslation->id
-            ])
-
-            @include('partials.forms.input-hidden', [
-                  'name' => 'category_id',
-                  'value' => $categoryId,
-            ])
-            @include('partials.forms.input-hidden', [
-                  'name' => 'language_code',
-                  'value' => $languageCode
-            ])
-
-         <div class="row">
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => 'Name',
-                    'name' => 'name',
-                    'placeholder' => 'Category name',
-                    'value' => $categoryTranslation->name,
-                ])
+    <div class="container max-w-md px-0">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <h2>@lang('views.edit_translation')</h2>
             </div>
-            
-            <div class="col-12">
-                @include('partials.forms.textarea', [
-                      'title' => __('general.description'),
-                      'name' => 'description',
-                      'placeholder' => 'Description',
-                      'value' => $categoryTranslation->description,
-                ])
+            <div class="col-12 col-sm-6 text-right">
+                <span class="badge badge-secondary">{{$selectedLocaleName}}</span>
             </div>
         </div>
 
-        @include('partials.forms.buttons-back-submit', [
-            'route' => 'categories.index'  
+        @include('partials.forms.error-management', [
+              'style' => 'alert-danger',
         ])
 
-    </form>
-    
-    <div class="row mt-2">  
-        <div class="col-12 action">
-            <form action="{{ route('categoryTranslations.destroy',$categoryTranslation->id) }}" method="POST">
+        <form action="{{ route('categoryTranslations.update') }}" method="POST">
+            @csrf
+            @method('PUT')
 
-                @csrf
-                @method('DELETE')
+                @include('partials.forms.input-hidden', [
+                      'name' => 'category_translation_id',
+                      'value' => $categoryTranslation->id
+                ])
 
-                <button type="submit" class="btn btn-danger float-right"><i class="far fa-trash-alt"></i></button>
-            </form>
+                @include('partials.forms.input-hidden', [
+                      'name' => 'category_id',
+                      'value' => $categoryId,
+                ])
+                @include('partials.forms.input-hidden', [
+                      'name' => 'language_code',
+                      'value' => $languageCode
+                ])
+
+             <div class="row">
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => 'Name',
+                        'name' => 'name',
+                        'placeholder' => 'Category name',
+                        'value' => $categoryTranslation->name,
+                    ])
+                </div>
+                
+                <div class="col-12">
+                    @include('partials.forms.textarea', [
+                          'title' => __('general.description'),
+                          'name' => 'description',
+                          'placeholder' => 'Description',
+                          'value' => $categoryTranslation->description,
+                    ])
+                </div>
+            </div>
+
+            @include('partials.forms.buttons-back-submit', [
+                'route' => 'categories.index'  
+            ])
+
+        </form>
+        
+        <div class="row mt-2">  
+            <div class="col-12 action">
+                <form action="{{ route('categoryTranslations.destroy',$categoryTranslation->id) }}" method="POST">
+
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger float-right"><i class="far fa-trash-alt"></i></button>
+                </form>
+            </div>
         </div>
     </div>
 
--- resources/views/postTranslations/edit.blade.php
+++ resources/views/postTranslations/edit.blade.php
@@ -2,86 +2,88 @@
 
 
 @section('content')
+    <div class="container max-w-md px-0">
 
-    <div class="row">
-        <div class="col-12 col-sm-6">
-            <h2>@lang('views.edit_translation')</h2>
-        </div>
-        <div class="col-12 col-sm-6 text-right">
-            <span class="badge badge-secondary">{{$selectedLocaleName}}</span>
-        </div>
-    </div>
-
-    @include('partials.forms.error-management', [
-          'style' => 'alert-danger',
-    ])
-
-    <form action="{{ route('postTranslations.update',$postId) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        @include('partials.forms.input-hidden', [
-              'name' => 'post_translation_id',
-              'value' => $postTranslation->id
-        ])
-
-        @include('partials.forms.input-hidden', [
-              'name' => 'post_id',
-              'value' => $postId
-        ])
-        @include('partials.forms.input-hidden', [
-              'name' => 'language_code',
-              'value' => $languageCode
-        ])
-
-         <div class="row">
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => 'Title',
-                    'name' => 'title',
-                    'placeholder' => 'Post title',
-                    'value' => $postTranslation->title
-                ])
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <h2>@lang('views.edit_translation')</h2>
             </div>
-            <div class="col-12">
-                @include('partials.forms.textarea-plain', [
-                    'title' =>  __('views.before_post_contents'),
-                    'name' => 'before_content',
-                    'value' => $postTranslation->before_content,
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.textarea-post', [
-                    'title' => 'Text',
-                    'name' => 'body',
-                    'placeholder' => 'Post text',
-                    'value' => $postTranslation->body
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.textarea-plain', [
-                    'title' =>  __('views.after_post_contents'),
-                     'name' => 'after_content',
-                     'value' => $postTranslation->after_content,
-                ])
+            <div class="col-12 col-sm-6 text-right">
+                <span class="badge badge-secondary">{{$selectedLocaleName}}</span>
             </div>
         </div>
 
-        @include('partials.forms.buttons-back-submit', [
-            'route' => 'posts.index'  
+        @include('partials.forms.error-management', [
+              'style' => 'alert-danger',
         ])
 
-    </form>
+        <form action="{{ route('postTranslations.update',$postId) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-    <div class="row mt-2">  
-        <div class="col-12 action">
-            <form action="{{ route('postTranslations.destroy',$postTranslation->id) }}" method="POST">
+            @include('partials.forms.input-hidden', [
+                  'name' => 'post_translation_id',
+                  'value' => $postTranslation->id
+            ])
 
-                @csrf
-                @method('DELETE')
+            @include('partials.forms.input-hidden', [
+                  'name' => 'post_id',
+                  'value' => $postId
+            ])
+            @include('partials.forms.input-hidden', [
+                  'name' => 'language_code',
+                  'value' => $languageCode
+            ])
 
-                <button type="submit" class="btn btn-danger float-right"><i class="far fa-trash-alt"></i></button>
-            </form>
+             <div class="row">
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => 'Title',
+                        'name' => 'title',
+                        'placeholder' => 'Post title',
+                        'value' => $postTranslation->title
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea-plain', [
+                        'title' =>  __('views.before_post_contents'),
+                        'name' => 'before_content',
+                        'value' => $postTranslation->before_content,
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea-post', [
+                        'title' => 'Text',
+                        'name' => 'body',
+                        'placeholder' => 'Post text',
+                        'value' => $postTranslation->body
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea-plain', [
+                        'title' =>  __('views.after_post_contents'),
+                         'name' => 'after_content',
+                         'value' => $postTranslation->after_content,
+                    ])
+                </div>
+            </div>
+
+            @include('partials.forms.buttons-back-submit', [
+                'route' => 'posts.index'  
+            ])
+
+        </form>
+
+        <div class="row mt-2">  
+            <div class="col-12 action">
+                <form action="{{ route('postTranslations.destroy',$postTranslation->id) }}" method="POST">
+
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger float-right"><i class="far fa-trash-alt"></i></button>
+                </form>
+            </div>
         </div>
     </div>
 
