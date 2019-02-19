7d40d76e2896df24559bcc50028f4871613aa694
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-19 19:10:43 +0100

    fixed teacher profile

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -11,26 +11,49 @@
 
     <div class="row">
         <div class="container max-w-md px-0">
-            <div class="teacherName col-12 mb-5">
-                <h2>{{ $teacher->name }}</h2>
+            <div class="teacherName col-12 mb-4">
+                <h3>{{ $teacher->name }}</h3>
             </div>
-
+            <div class="col-12">
+                @if(!empty($country->name))
+                    <div class="row">
+                        <div class="col-12">
+                            <i data-toggle="tooltip" data-placement="top" title="" class="far fa-globe-americas mr-1 dark-gray" data-original-title="@lang('general.country')"></i>
+                            {{ $country->name }}
+                        </div>
+                    </div> 
+                @endif
+                @if(!empty($teacher->year_starting_practice))<p><b>Year of starting to practice: </b>{{ $teacher->year_starting_practice }}</p>@endif
+                @if(!empty($teacher->year_starting_teach))<p><b>Year of starting to teach:</b>{{ $teacher->year_starting_teach }}</p>@endif
+                
+                <div class="row mt-2">
+                @if(!empty($teacher->facebook))
+                    <div class="col-6">
+                        <i title="" class="fab fa-facebook-square mr-1 dark-gray"></i>
+                        <a href="{{ $teacher->facebook }}" target="_blank">@lang('views.facebook_profile')</a>
+                    </div>
+                @endif
+                @if(!empty($teacher->website))
+                    <div class="col-6">
+                        <i class="fas fa-globe mr-1 dark-gray"></i>
+                        <a href="{{ $teacher->website }}" target="_blank">@lang('views.website')</a>
+                    </div> 
+                @endif 
+                </div>
+            </div>
+            
             @if(!empty($teacher->bio))
-                <div class="teacherBio col-12">
-                    <h3>Bio</h3>
+                <div class="teacherBio col-12 mt-5">
+                    <h4>Bio</h4>
                     <img class="teacherPhoto ml-3" alt="{{ $teacher->name }}" src="/storage/images/teachers_profile/thumb_{{ $teacher->profile_picture }}" style="width:345px; float:right;">
                     {!! $teacher->bio !!}
                 </div>
             @endif
 
-            <div class="col-12 mt-4">
-                @if(!empty($country->name))<p><b>Country: </b> {{ $country->name }}</p>@endif
-                @if(!empty($teacher->year_starting_practice))<p><b>Year of starting to practice: </b>{{ $teacher->year_starting_practice }}</p>@endif
-                @if(!empty($teacher->year_starting_teach))<p><b>Year of starting to teach:</b>{{ $teacher->year_starting_teach }}</p>@endif
-                @if(!empty($teacher->facebook))<p><b>Facebook profile: </b><a href="{{ $teacher->facebook }}" target="_blank">{{ $teacher->facebook }}</a></p>@endif
-                @if(!empty($teacher->website))<p><b>Website: </b><a href="{{ $teacher->website }}" target="_blank">{{ $teacher->website }}</a></p>@endif        
-            </div>
+            
         </div>
+        
+        
 
         @if(count($eventsTeacherWillTeach))
             <div class="col-12 mt-5">
