1706d0f8f65f6756b366233ff9021285a0dcc90a
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-19 09:30:55 +0100

    refactored categories index

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -2,45 +2,45 @@
 
 
 @section('content')
+    <div class="container max-w-md px-0">
     
-    <div class="row">
-        <div class="col-12 col-sm-6">
-            <h3>@lang('views.add_new_category')</h3>
-        </div>
-        <div class="col-12 col-sm-6 text-right">
-            <span class="badge badge-secondary">English</span>
-        </div>
-    </div>
-
-    @include('partials.forms.error-management', [
-          'style' => 'alert-danger',
-    ])
-
-    <form action="{{ route('categories.store') }}" method="POST">
-        @csrf
-
-         <div class="row">
-            <div class="col-12">
-                @include('partials.forms.input', [
-                      'title' => __('general.name'),
-                      'name' => 'name',
-                      'placeholder' => 'Category name'
-                ])
+        <div class="row">
+            <div class="col-12 col-sm-10">
+                <h3>@lang('views.add_new_category')</h3>
             </div>
-            <div class="col-12">
-                @include('partials.forms.textarea', [
-                      'title' => __('general.description'),
-                      'name' => 'description',
-                      'placeholder' => 'Description'
-                ])
+            <div class="col-12 col-sm-2 text-right">
+                <span class="badge badge-secondary">English</span>
             </div>
         </div>
 
-        @include('partials.forms.buttons-back-submit', [
-            'route' => 'categories.index'  
+        @include('partials.forms.error-management', [
+              'style' => 'alert-danger',
         ])
 
-    </form>
+        <form action="{{ route('categories.store') }}" method="POST">
+            @csrf
 
+             <div class="row">
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                          'title' => __('general.name'),
+                          'name' => 'name',
+                          'placeholder' => 'Category name'
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea', [
+                          'title' => __('general.description'),
+                          'name' => 'description',
+                          'placeholder' => 'Description'
+                    ])
+                </div>
+            </div>
 
+            @include('partials.forms.buttons-back-submit', [
+                'route' => 'categories.index'  
+            ])
+
+        </form>
+    </div>
 @endsection
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,91 +2,60 @@
 
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>@lang('views.category_management')</h2>
-            </div>
-            <div class="pull-right mt-4 float-right">
+    <div class="container max-w-md px-0">
+        <div class="row">
+            <div class="col-12 col-sm-7">
+                <h3>@lang('views.category_management')</h3>
+            </div>    
+            <div class="col-12 col-sm-5 mt-sm-0 text-right">
                 <a class="btn btn-success create-new" href="{{ route('categories.create') }}"><i class="fa fas fa-plus-circle"></i> @lang('views.create_new_category')</a>
             </div>
         </div>
-    </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success mt-4">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
-
-    {{-- List of post categories --}}
-    <div class="venuesList my-4">
-        @foreach ($categories as $category)
-            <div class="row p-1 {{ $loop->index % 2 ? 'bg-light': 'bg-white' }}">
-                
-                {{-- Title --}}
-                    <div class="col-5 py-2 title">
-                        <a href="{{ route('categories.edit',$category->id) }}">{{ $category->translate('en')->name }}</a>
-                    </div>
-                
-                {{-- Translations --}}
-                    <div class="col-5 text-right translation mt-1">
-                        @foreach ($countriesAvailableForTranslations as $key => $countryAvTrans)
-                            @if($category->hasTranslation($key))
-                                <a href="/categoryTranslations/{{ $category->id }}/{{ $key }}/edit" class="bg-success text-white px-2 py-1 mb-1 mb-lg-0 d-inline-block rounded">{{$key}}</a>
-                            @else
-                                <a href="/categoryTranslations/{{ $category->id }}/{{ $key }}/create" class="bg-secondary text-white px-2 py-1 mb-1 mb-lg-0 d-inline-block rounded">{{$key}}</a>
-                            @endif
-                        @endforeach
-                    </div>
-                
-                <div class="col-2 action">
-                    <form action="{{ route('categories.destroy',$category->id) }}" method="POST">
-
-                        @csrf
-                        @method('DELETE')
-
-                        <button type="submit" class="btn btn-danger float-right"><i class="far fa-trash-alt"></i></button>
-                    </form>
-                </div>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success mt-4">
+                <p>{{ $message }}</p>
             </div>
-        @endforeach    
+        @endif
+
+        {{-- List of post categories --}}
+        <div class="venuesList my-4">
+            @foreach ($categories as $category)
+                <div class="row bg-white shadow-1 rounded mb-3 pb-2 pt-3 mx-1">
+                    {{-- Title --}}
+                        <div class="col-12 py-1 title">
+                            <h5 class="darkest-gray">{{ $category->translate('en')->name }}</h5>
+                        </div>
+                        
+                    {{-- Translations --}}
+                        <div class="col-12 mb-4 mt-4">
+                            @foreach ($countriesAvailableForTranslations as $key => $countryAvTrans)
+                                @if($category->hasTranslation($key))
+                                    <a href="/categoryTranslations/{{ $category->id }}/{{ $key }}/edit" class="bg-success text-white px-2 py-1 mb-1 mb-lg-0 d-inline-block rounded">{{$key}}</a>
+                                @else
+                                    <a href="/categoryTranslations/{{ $category->id }}/{{ $key }}/create" class="bg-secondary text-white px-2 py-1 mb-1 mb-lg-0 d-inline-block rounded">{{$key}}</a>
+                                @endif
+                            @endforeach
+                        </div>
+                        
+                    {{-- Buttons --}}
+                        <div class="col-12 pb-2 action">
+                            <form action="{{ route('categories.destroy',$category->id) }}" method="POST">
+
+                                <a class="btn btn-primary float-right" href="{{ route('categories.edit',$category->id) }}">@lang('views.edit')</a>
+                                
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit" class="btn btn-link pl-0">@lang('views.delete')</button>
+                            </form>
+                        </div>
+                </div>
+                
+            @endforeach    
+        </div>
+        
+        {!! $categories->links() !!}
     </div>
-    {{-- List of post categories --}}
-    {{-- <table class="table table-bordered mt-4">
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Slug</th>
-            <th width="280">Action</th>
-        </tr>
-        @foreach ($categories as $category)
-        <tr>
-            <td>{{ $category->id }}</td>
-            <td>{{ $category->name }}</td>
-            <td>{{ $category->slug }}</td>
-            <td>
-                <form action="{{ route('categories.destroy',$category->id) }}" method="POST">
-
-
-                    <a class="btn btn-info" href="{{ route('categories.show',$category->id) }}">Show</a>
-                    <a class="btn btn-primary" href="{{ route('categories.edit',$category->id) }}">Edit</a>
-
-
-                    @csrf
-                    @method('DELETE')
-
-
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>--}}
-
-
-    {!! $categories->links() !!}
-
 
 @endsection
