f040915792b67e2b46079ab5dfce02153b8fc541
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-19 10:26:42 +0100

    teacher create and edit restyled

--- resources/views/eventVenues/create.blade.php
+++ resources/views/eventVenues/create.blade.php
@@ -2,108 +2,109 @@
 
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <h2>@lang('views.add_new_venue')</h2>
-        </div>
-    </div>
-
-    @include('partials.forms.error-management', [
-        'style' => 'alert-danger',
-    ])
-
-    <form action="{{ route('eventVenues.store') }}" method="POST">
-        @csrf
-
-         <div class="row">
+    <div class="container max-w-md px-0">
+        <div class="row">
             <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('general.name'),
-                    'name' => 'name',
-                    'placeholder' => __('homepage-serach.venue_name'),
-                    'value' => old('name')
-                ])
-            </div>
-
-            {{-- Show the created by field just to the admin and super admin --}}
-            @if(empty($authorUserId))
-                <div class="col-12">
-                    @include('partials.forms.select', [
-                        'title' => __('views.created_by'),
-                        'name' => 'created_by',
-                        'placeholder' => __('views.select_owner'),
-                        'records' => $users,
-                        'liveSearch' => 'true',
-                        'mobileNativeMenu' => false,
-                    ])
-                </div>
-            @endif
-
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('views.street'),
-                    'name' => 'address',
-                    'placeholder' => '',
-                    'value' => old('address')
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('views.city'),
-                    'name' => 'city',
-                    'placeholder' => '',
-                    'value' => old('city')
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('views.state_province'),
-                    'name' => 'state_province',
-                    'placeholder' => '',
-                    'value' => old('state_province')
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.select', [
-                      'title' => __('views.country'),
-                      'name' => 'country_id',
-                      'placeholder' => __('views.select_country'), 
-                      'records' => $countries,
-                      'liveSearch' => 'true',
-                      'mobileNativeMenu' => false,
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('views.zip_code'),
-                    'name' => 'zip_code',
-                    'placeholder' => '',
-                    'value' => old('zip_code')
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('views.website'),
-                    'name' => 'website',
-                    'placeholder' => 'https://...',
-                    'value' => old('website')
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.textarea', [
-                    'title' => __('general.description'),
-                    'name' => 'description',
-                    'placeholder' => '',
-                    'value' => old('description')
-                ])
+                <h2>@lang('views.add_new_venue')</h2>
             </div>
         </div>
 
-        @include('partials.forms.buttons-back-submit', [
-            'route' => 'eventVenues.index'  
+        @include('partials.forms.error-management', [
+            'style' => 'alert-danger',
         ])
 
-    </form>
+        <form action="{{ route('eventVenues.store') }}" method="POST">
+            @csrf
 
+             <div class="row">
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('general.name'),
+                        'name' => 'name',
+                        'placeholder' => __('homepage-serach.venue_name'),
+                        'value' => old('name')
+                    ])
+                </div>
+
+                {{-- Show the created by field just to the admin and super admin --}}
+                @if(empty($authorUserId))
+                    <div class="col-12">
+                        @include('partials.forms.select', [
+                            'title' => __('views.created_by'),
+                            'name' => 'created_by',
+                            'placeholder' => __('views.select_owner'),
+                            'records' => $users,
+                            'liveSearch' => 'true',
+                            'mobileNativeMenu' => false,
+                        ])
+                    </div>
+                @endif
+
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('views.street'),
+                        'name' => 'address',
+                        'placeholder' => '',
+                        'value' => old('address')
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('views.city'),
+                        'name' => 'city',
+                        'placeholder' => '',
+                        'value' => old('city')
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('views.state_province'),
+                        'name' => 'state_province',
+                        'placeholder' => '',
+                        'value' => old('state_province')
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.select', [
+                          'title' => __('views.country'),
+                          'name' => 'country_id',
+                          'placeholder' => __('views.select_country'), 
+                          'records' => $countries,
+                          'liveSearch' => 'true',
+                          'mobileNativeMenu' => false,
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('views.zip_code'),
+                        'name' => 'zip_code',
+                        'placeholder' => '',
+                        'value' => old('zip_code')
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('views.website'),
+                        'name' => 'website',
+                        'placeholder' => 'https://...',
+                        'value' => old('website')
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea', [
+                        'title' => __('general.description'),
+                        'name' => 'description',
+                        'placeholder' => '',
+                        'value' => old('description')
+                    ])
+                </div>
+            </div>
+
+            @include('partials.forms.buttons-back-submit', [
+                'route' => 'eventVenues.index'  
+            ])
+
+        </form>
+    </div>
 
 @endsection
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,100 +2,101 @@
 
 
 @section('content')
-
-    <div class="row">
-        <div class="col-12 col-sm-6">
-            <h2>@lang('views.edit_post')</h2>
-        </div>
-        <div class="col-12 col-sm-6 text-right">
-            <span class="badge badge-secondary">English</span>
-        </div>
-    </div>
-
-    @include('partials.forms.error-management', [
-          'style' => 'alert-danger',
-    ])
-
-    <form action="{{ route('posts.update',$post->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-
-         <div class="row">
-            <div class="col-12">
-                @include('partials.forms.input', [
-                    'title' => __('views.title'),
-                    'name' => 'title',
-                    'placeholder' => 'Event title',
-                    'value' => $post->translate('en')->title
-                ])
+    <div class="container max-w-md px-0">
+        <div class="row mb-4">
+            <div class="col-12 col-sm-6">
+                <h2>@lang('views.edit_post')</h2>
+            </div>
+            <div class="col-12 col-sm-6 text-right">
+                <span class="badge badge-secondary">English</span>
             </div>
         </div>
-        <div class="row">
-            <div class="col-10">
-                @include('partials.forms.input-readonly', [
-                    'title' => 'Link to this post',
-                    'name' => 'post_link',
-                    'tooltip' => '',
-                    'value' => env('APP_URL').'post/'.$post->translate('en')->slug
-                ])
-            </div>
-            <div class="col-2">
-                @include('partials.forms.input-readonly', [
-                    'title' => 'Post ID',
-                    'name' => 'post_id',
-                    'tooltip' => '',
-                    'value' => $post->id
-                ])
-            </div>
-            
-        </div>
-        <div class="row">
-            <div class="col-12">
-                @include('partials.forms.select', [
-                    'title' => __('views.category'),
-                    'name' => 'category_id',
-                    'placeholder' => __('views.select_category'),
-                    'records' => $categories,
-                    'seleted' => $post->category_id,
-                    'liveSearch' => 'false',
-                    'mobileNativeMenu' => true,
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.textarea-plain', [
-                    'title' =>  __('views.before_post_contents'),
-                    'name' => 'before_content',
-                    'value' => $post->translate('en')->before_content,
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.textarea-post', [
-                    'title' => 'Text',
-                    'name' => 'body',
-                    'placeholder' => 'Post text',
-                    'value' => $post->translate('en')->body
-                ])
-            </div>
-            <div class="col-12">
-                @include('partials.forms.textarea-plain', [
-                    'title' =>  __('views.after_post_contents'),
-                    'name' => 'after_content',
-                    'value' => $post->translate('en')->after_content,
-                ])
-            </div>
-            
-            @include('partials.forms.upload-image', [
-                'title' => __('views.upload_profile_picture'), 
-                'name' => 'introimage',
-                'folder' => 'posts_intro_images',
-                'value' => $post->introimage,
-            ])            
-        </div>
 
-        @include('partials.forms.buttons-back-submit', [
-            'route' => 'posts.index'  
+        @include('partials.forms.error-management', [
+              'style' => 'alert-danger',
         ])
 
-    </form>
+        <form action="{{ route('posts.update',$post->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
 
+             <div class="row">
+                <div class="col-12">
+                    @include('partials.forms.input', [
+                        'title' => __('views.title'),
+                        'name' => 'title',
+                        'placeholder' => 'Event title',
+                        'value' => $post->translate('en')->title
+                    ])
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-10">
+                    @include('partials.forms.input-readonly', [
+                        'title' => 'Link to this post',
+                        'name' => 'post_link',
+                        'tooltip' => '',
+                        'value' => env('APP_URL').'post/'.$post->translate('en')->slug
+                    ])
+                </div>
+                <div class="col-2">
+                    @include('partials.forms.input-readonly', [
+                        'title' => 'Post ID',
+                        'name' => 'post_id',
+                        'tooltip' => '',
+                        'value' => $post->id
+                    ])
+                </div>
+                
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    @include('partials.forms.select', [
+                        'title' => __('views.category'),
+                        'name' => 'category_id',
+                        'placeholder' => __('views.select_category'),
+                        'records' => $categories,
+                        'seleted' => $post->category_id,
+                        'liveSearch' => 'false',
+                        'mobileNativeMenu' => true,
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea-plain', [
+                        'title' =>  __('views.before_post_contents'),
+                        'name' => 'before_content',
+                        'value' => $post->translate('en')->before_content,
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea-post', [
+                        'title' => 'Text',
+                        'name' => 'body',
+                        'placeholder' => 'Post text',
+                        'value' => $post->translate('en')->body
+                    ])
+                </div>
+                <div class="col-12">
+                    @include('partials.forms.textarea-plain', [
+                        'title' =>  __('views.after_post_contents'),
+                        'name' => 'after_content',
+                        'value' => $post->translate('en')->after_content,
+                    ])
+                </div>
+                
+                @include('partials.forms.upload-image', [
+                    'title' => __('views.upload_profile_picture'), 
+                    'name' => 'introimage',
+                    'folder' => 'posts_intro_images',
+                    'value' => $post->introimage,
+                ])            
+            </div>
+
+            @include('partials.forms.buttons-back-submit', [
+                'route' => 'posts.index'  
+            ])
+
+        </form>
+    </div>
+    
 @endsection
