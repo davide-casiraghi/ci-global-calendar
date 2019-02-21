a8703b0da988f7c96f0a0b1dc428abf455625718
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-21 18:38:40 +0100

    fixed user show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,20 +1,25 @@
 @extends('teachers.layout')
 
 @section('content')
-
-    <div class="row">
-        <div class="col-12 mb-5">
-            <h2>{{ $user->name }}</h2>
+    <div class="container max-w-md px-0">
+        <div class="row m-0 p-4 white-bg rounded">
+            <div class="col-12">
+                
+                <div class="row">
+                    <div class="col-12 mt-3">
+                        <h4>{{ $user->name }}</h4>
+                    </div>
+                    <div class="col-12 mt-2">
+                        <i data-toggle="tooltip" data-placement="top" title="" class="far fa-globe-americas mr-1 dark-gray" data-original-title="@lang('general.country')"></i>
+                        {{ $countries[$user->country_id] }}
+                    </div>
+                    <div class="col-12 mt-5">
+                        {!! $user->description !!}
+                    </div>
+                    
+                </div>
+            </div>
+            
         </div>
-        <div class="col-12 mt-4">
-            <h3>Country</h3>
-            {{ $countries[$user->country_id] }}
-        </div>
-        <div class="col-12 mt-4">
-            <h3>Description</h3>
-            {!! $user->description !!}
-        </div>
-
     </div>
-
 @endsection
