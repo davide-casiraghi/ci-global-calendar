3cc5d68f8ee5f594053bff4b457cd04b710763f6
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-22 12:42:05 +0100

    added translations

--- resources/views/eventVenues/index.blade.php
+++ resources/views/eventVenues/index.blade.php
@@ -14,7 +14,7 @@
     <div class="container max-w-md px-0">
         <div class="row">
             <div class="col-12 col-sm-7">
-                <h3>@lang('views.events_venue_management')</h3>
+                <h4>@lang('views.events_venue_management')</h4>
             </div>
             <div class="col-12 col-sm-5 mt-4 mt-sm-0 text-right">
                 <a class="btn btn-success create-new" href="{{ route('eventVenues.create') }}"><i class="fa fas fa-plus-circle"></i> @lang('views.create_new_venue')</a>
