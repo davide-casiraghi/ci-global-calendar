01d3c74f4c46a7fcca36e924d1c5e41c448f7903
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-23 08:34:31 +0100

    created country routes

--- app/Http/Resources/Country.php
+++ app/Http/Resources/Country.php
@@ -14,6 +14,11 @@ class Country extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'code' => $this->code,
+            'continent_id' => $this->continent_id,
+        ];
     }
 }
