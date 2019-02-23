dc91950e108d2f8f9f0c9948673c86df46c2fc0a
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-23 22:03:43 +0100

    created model for donations

--- app/DonationOffer.php
+++ app/DonationOffer.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class DonationOffer extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'surname', 'email', 'country_id', 'contact_trough_voip', 'language_spoken', 'offer_kind', 'gift_kind', 'gift_description', 'volunteer_kind', 'volunteer_description', 'other_description', 'suggestions'
+    ];
 }
