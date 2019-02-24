3a98afe1a80aee3d99dedf0458fcf7f936566be1
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-24 15:56:39 +0100

    now cards are green when activated

--- resources/views/partials/forms/input-radio-cards.blade.php
+++ resources/views/partials/forms/input-radio-cards.blade.php
@@ -19,7 +19,7 @@
     <div class="row mt-2">
         <div class="col-6 col-sm-3 col-md-6 col-lg-3 mb-2 text-center form-check">
             
-            <label class="form-check-label bg-white shadow-1 rounded p-2 w-100 h-100" for="offerFinancial">
+            <label class="form-check-label bg-white shadow-1 rounded px-1 py-2 w-100 h-100" for="offerFinancial">
                 <input id="offerFinancial" class="form-check-input d-none" type="radio" name="offer_kind" value="1" />
                 <i class="far fa-hand-holding-usd text-xl"></i>
                 <span class="dark-gray text-xs d-block text-uppercase mt-2">@lang('views.donation_kind_financial')</span>
@@ -27,7 +27,7 @@
         </div>
         <div class="col-6 col-sm-3 col-md-6 col-lg-3 mb-2 text-center form-check">
             
-            <label class="form-check-label bg-white shadow-1 rounded p-2 w-100 h-100"for="offerGift">
+            <label class="form-check-label bg-white shadow-1 rounded px-1 py-2 w-100 h-100"for="offerGift">
                 <input id="offerGift" class="form-check-input d-none" type="radio" name="offer_kind" value="2" />
                 <i class="far fa-gift text-xl"></i>
                 <span class="dark-gray text-xs d-block text-uppercase mt-2">@lang('views.donation_kind_gift')</span>
@@ -35,7 +35,7 @@
         </div>
         <div class="col-6 col-sm-3 col-md-6 col-lg-3 mb-2 text-center form-check">
             
-            <label class="form-check-label bg-white shadow-1 rounded p-2 w-100 h-100"for="offerVolunteer">
+            <label class="form-check-label bg-white shadow-1 rounded px-1 py-2 w-100 h-100"for="offerVolunteer">
                 <input id="offerVolunteer" class="form-check-input d-none" type="radio" name="offer_kind" value="3" />
                 <i class="far fa-hands-helping text-xl"></i>
                 <span class="dark-gray text-xs d-block text-uppercase mt-2">@lang('views.donation_kind_volunteer')</span>
@@ -43,7 +43,7 @@
         </div>
         <div class="col-6 col-sm-3 col-md-6 col-lg-3 mb-2 text-center form-check">
             
-            <label class="form-check-label bg-white shadow-1 rounded p-2 w-100 h-100"for="offerOther">
+            <label class="form-check-label bg-white shadow-1 rounded px-1 py-2 w-100 h-100"for="offerOther">
                 <input id="offerOther" class="form-check-input d-none" type="radio" name="offer_kind" value="4" />
                 <i class="far fa-hands-heart text-xl"></i>
                 <span class="dark-gray text-xs d-block text-uppercase mt-2">@lang('views.donation_kind_other')</span>
