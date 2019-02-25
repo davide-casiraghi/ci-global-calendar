9d288629780bd483cf5af9681447df4ae6caa376
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-02-25 07:07:32 +0100

    styled color badges

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,7 +14,7 @@
     <div class="container max-w-md px-0">
         <div class="row">
             <div class="col-12 col-sm-7">
-                <h3>@lang('views.users_management')</h3>
+                <h4>@lang('views.users_management')</h4>
             </div>
             <div class="col-12 col-sm-5 mt-4 mt-sm-0 text-right">
                 <a class="btn btn-success create-new" href="{{ route('users.create') }}"><i class="fa fas fa-plus-circle"></i> @lang('views.create_new_user')</a>
