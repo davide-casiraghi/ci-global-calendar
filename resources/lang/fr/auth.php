2f8fe63b933549103e53d4c310d692d228e664ff
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-13 09:01:02 +0100

    added some translations

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -16,4 +16,5 @@ return [
     'failed' => 'Ces références ne correspondent pas à nos dossiers.',
     'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans quelques secondes.',
     'successfully_registered' => 'Vous êtes bien enregistré en tant qu\'utilisateur-trice du CI Global Calendar. Vous recevrez un  courriel de confirmation une fois votre compte approuvé par l\'administrateur du site. Merci de nous rejoindre!',
+    'not_allowed_to_access' => 'Vous n\'êtes pas autorisé à accéder à cette ressource',
 ];
