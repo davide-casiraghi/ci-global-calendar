5ec19129f9656668bd1d2b4b34c0364b3093df8c
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-24 15:27:23 +0100

    added to statistics - that they are updated at midnight

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -14,8 +14,10 @@
     </div>
     
     
-    {{-- USERS --}}
+    
     <div class="row">
+        
+        {{-- USERS --}}
         <div class="col-12 col-sm-6 col-lg-3">
             <div class="card mb-2">
                 <div class="card-body">
@@ -98,6 +100,12 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-12 dark-gray text-xs">
+            The statistics are updated every day at midnight.
         </div>
     </div>
 
