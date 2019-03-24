3dfe171a572049df1148678625648ec4567b4305
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-24 11:21:08 +0100

    Apply fixes from StyleCI (#15)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,10 +2,9 @@
 
 namespace App\Console;
 
+use App\Statistic;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
-use App\Statistic;
 
 class Kernel extends ConsoleKernel
 {
@@ -28,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        
+
         $schedule->call(function () {
             Statistic::updateStatistics();
         })->daily();
--- app/Statistic.php
+++ app/Statistic.php
@@ -4,12 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-use DB;
-use App\User;
-use App\Teacher;
-use App\Organizer;
-use App\Event;
-
 class Statistic extends Model
 {
     /***************************************************************************/
@@ -26,15 +20,14 @@ class Statistic extends Model
         'registered_users_number', 'organizers_number', 'teachers_number', 'active_events_number',
     ];
 
-
     public static function updateStatistics()
     {
         $todayDate = Carbon::now()->format('d-m-Y');
-        $lastUpdateStatistic = Statistic::find(\DB::table('statistics')->max('id'));
+        $lastUpdateStatistic = self::find(\DB::table('statistics')->max('id'));
         $lastUpdateDate = ($lastUpdateStatistic != null) ? $lastUpdateStatistic->created_at->format('d-m-Y') : null;
-        
-        if ($lastUpdateDate != $todayDate){
-            $statistics = new Statistic();
+
+        if ($lastUpdateDate != $todayDate) {
+            $statistics = new self();
             $statistics->registered_users_number = User::count();
             $statistics->organizers_number = Organizer::count();
             $statistics->teachers_number = Teacher::count();
@@ -43,5 +36,4 @@ class Statistic extends Model
             $statistics->save();
         }
     }
-
 }
