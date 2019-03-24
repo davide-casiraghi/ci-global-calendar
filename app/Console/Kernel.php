60b9afd7efc304eb036255bfa0cdf30cc6435cd3
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-24 11:29:30 +0100

    refactored code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
         
         $schedule->call(function () {
             Statistic::updateStatistics();
