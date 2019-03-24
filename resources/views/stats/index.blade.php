93ff4ea95b64218183f916860151969882afe92d
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-24 08:39:54 +0100

    added blue colors

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -6,8 +6,107 @@
 @stop
 
 @section('content')
-    stats view
     
-    {{$statsDatas['registeredUsersNumber']}}
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <h3>CIGC statitics</h3>
+        </div>
+    </div>
+    
+    
+    {{-- USERS --}}
+    <div class="row">
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="row">
+                            <div class="col max-w-50-px">
+                                <i class="fas fa-user-alt text-5xl dark-gray"></i>
+                            </div>
+                            <div class="col pl-4">
+                                <div class="text-xs text-uppercase dark-gray font-weight-bold">Registered users</div>
+                                <div class="text-3xl font-weight-bolder">
+                                    {{$statsDatas['registeredUsersNumber']}}
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        {{-- Orgenizers --}}
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="row">
+                            <div class="col max-w-70-px">
+                                <i class="fas fa-users text-5xl"></i>
+                            </div>
+                            <div class="col pl-4">
+                                <div class="text-xs text-uppercase dark-gray font-weight-bold">Organier profiles</div>
+                                <div class="text-3xl font-weight-bolder">
+                                    {{$statsDatas['organizersNumber']}}
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        {{-- Teachers --}}
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="row">
+                            <div class="col max-w-70-px">
+                                <i class="far fa-users text-5xl"></i>
+                            </div>
+                            <div class="col pl-4">
+                                <div class="text-xs text-uppercase dark-gray font-weight-bold">Teacher profiles</div>
+                                <div class="text-3xl font-weight-bolder">
+                                    {{$statsDatas['teachersNumber']}}
+                                </div>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        {{-- Active events --}}
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="row">
+                            <div class="col max-w-50-px">
+                                <i class="far fa-calendar-alt text-5xl"></i>
+                            </div>
+                            <div class="col pl-4">
+                                <div class="text-xs text-uppercase dark-gray font-weight-bold">Active events</div>
+                                <div class="text-3xl font-weight-bolder">
+                                    {{$statsDatas['activeEventsNumber']}}
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
 
 @endsection
