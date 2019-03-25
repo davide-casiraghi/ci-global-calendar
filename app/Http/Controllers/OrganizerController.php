6f650e28fa38adc59e051ada4a2f6f5758a97b3b
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-25 17:19:16 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/OrganizerController.php
+++ app/Http/Controllers/OrganizerController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Rule;
 
 class OrganizerController extends Controller
 {
