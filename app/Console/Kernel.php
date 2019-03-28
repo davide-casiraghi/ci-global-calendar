66b8e1eed83ed4fdf473074aa765e8cebac27479
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-28 01:24:42 +0100

    updated scheduler for statistics

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             Statistic::updateStatistics();
-        })->daily()
+        })->everyMinute()
         ->appendOutputTo($filePath)
         ->emailOutputTo(env('WEBMASTER_MAIL'));
     }
