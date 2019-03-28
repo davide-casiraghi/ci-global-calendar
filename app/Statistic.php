80fd67ae26083e6b5c81924f031c4b4d652f6443
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-28 01:18:23 +0100

    updated scheduler for statistics

--- app/Statistic.php
+++ app/Statistic.php
@@ -2,6 +2,12 @@
 
 namespace App;
 
+use App\User;
+use App\Event;
+use App\Teacher;
+use App\Organizer;
+use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Statistic extends Model
@@ -19,4 +25,26 @@ class Statistic extends Model
     protected $fillable = [
         'registered_users_number', 'organizers_number', 'teachers_number', 'active_events_number',
     ];
+
+    public static function updateStatistics(){
+        $todayDate = Carbon::now()->format('d-m-Y');
+        $lastUpdateStatistic = Statistic::find(\DB::table('statistics')->max('id'));
+        $lastUpdateDate = ($lastUpdateStatistic != null) ? $lastUpdateStatistic->created_at->format('d-m-Y') : null;
+
+        if ($lastUpdateDate != $todayDate) {
+            $statistics = new Statistic();
+            $statistics->registered_users_number = User::count();
+            $statistics->organizers_number = Organizer::count();
+            $statistics->teachers_number = Teacher::count();
+            $statistics->active_events_number = Event::getActiveEvents()->count();
+
+            $statistics->save();
+
+            return 'statistics updated';
+        } else {
+            return 'the statistics have been already updated today';
+        }
+    }
+
+
 }
