71c6ec971a8f1e0025d53255dfc5ef3c88d5fb1e
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-05 12:27:49 +0200

    Apply fixes from StyleCI (#86)

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Statistic;
 use App\User;
-use App\Teacher;
 use App\Event;
+use App\Teacher;
 use App\Organizer;
-use Illuminate\Http\Request;
+use App\Statistic;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-
 use App\Charts\LatestUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StatisticsController extends Controller
 {
@@ -37,7 +36,6 @@ class StatisticsController extends Controller
         $teachersByCountriesChart = $this->createTeachersByCountriesChart();
         $eventsByCountriesChart = $this->createEventsByCountriesChart();
         //$organizersByCountriesChart = $this->createOrganizersByCountriesChart();
-        
 
         return view('stats.index')
             ->with('statsDatas', $lastUpdateStatistic)
@@ -45,7 +43,7 @@ class StatisticsController extends Controller
             ->with('usersByCountryChart', $usersByCountryChart)
             ->with('teachersByCountriesChart', $teachersByCountriesChart)
             ->with('eventsByCountriesChart', $eventsByCountriesChart);
-            //->with('organizersByCountriesChart', $organizersByCountriesChart);
+        //->with('organizersByCountriesChart', $organizersByCountriesChart);
     }
 
     /***************************************************************************/
@@ -62,51 +60,50 @@ class StatisticsController extends Controller
     {
         Statistic::updateStatistics();
     }
-    
+
     /***************************************************************************/
+
     /**
-     * Create a LINE chart showing the number of users in the last x days
+     * Create a LINE chart showing the number of users in the last x days.
      *
      * @return App\Charts
      */
-    
-    public function createUsersNumberchart($daysRange){
-        
+    public function createUsersNumberchart($daysRange)
+    {
         $lastIDUpdatedStats = \DB::table('statistics')->max('id');
-    
+
         $data = collect([]); // Could also be an array
-        $labels = array();
+        $labels = [];
         for ($days_backwards = $daysRange; $days_backwards >= 0; $days_backwards--) {
-            $dayStat = Statistic::find($lastIDUpdatedStats-$days_backwards);
+            $dayStat = Statistic::find($lastIDUpdatedStats - $days_backwards);
             $data->push($dayStat->registered_users_number);
             $labels[] = Carbon::parse($dayStat->created_at)->format('d/m');
         }
-        
+
         $ret = new LatestUsers;
         $ret->labels($labels);
         $dataset = $ret->dataset('Users number', 'line', $data);
         /*$chart->labels(['One', 'Two', 'Three', 'Four']);
         $chart->dataset('My dataset', 'line', [1, 2, 3, 7]);
         $chart->dataset('My dataset 2', 'line', [4, 3, 2, 1]);*/
-    
-    
+
         //https://www.chartjs.org/docs/latest/charts/line.html
         $dataset->options([
             'borderColor' => '#2669A0',
         ]);
-        
+
         return $ret;
     }
-    
-    
+
     /***************************************************************************/
+
     /**
-     * Create a BAR chart showing the number of Users by country
+     * Create a BAR chart showing the number of Users by country.
      *
      * @return App\Charts
      */
-    
-    public function createUsersByCountryChart(){
+    public function createUsersByCountryChart()
+    {
         $usersByCountry = User::
                         leftJoin('countries', 'users.country_id', '=', 'countries.id')
                         ->select(DB::raw('count(*) as user_count, countries.name as country_name'))
@@ -114,71 +111,70 @@ class StatisticsController extends Controller
                         ->groupBy('country_id')
                         ->orderBy('country_name')
                         ->get();
-        
-        $data = collect([]); 
-        $labels = array();
+
+        $data = collect([]);
+        $labels = [];
         foreach ($usersByCountry as $key => $userByCountry) {
             $data->push($userByCountry->user_count);
             $labels[] = $userByCountry->country_name;
         }
-        
+
         $ret = new LatestUsers;
         $ret->labels($labels);
         $dataset = $ret->dataset('Users by Country', 'bar', $data);
-        
+
         //https://www.chartjs.org/docs/latest/charts/line.html
         $dataset->options([
             'borderColor' => '#2669A0',
         ]);
-    
+
         return $ret;
-    
     }
-    
+
     /***************************************************************************/
+
     /**
-     * Create a BAR chart showing the number of Teachers by country
+     * Create a BAR chart showing the number of Teachers by country.
      *
      * @return App\Charts
      */
-    
-    public function createTeachersByCountriesChart(){
-    
+    public function createTeachersByCountriesChart()
+    {
         $teachersByCountries = Teacher::
                         leftJoin('countries', 'teachers.country_id', '=', 'countries.id')
                         ->select(DB::raw('count(*) as teacher_count, countries.name as country_name'))
                         ->groupBy('country_id')
                         ->orderBy('country_name')
                         ->get();
-        
-        $data = collect([]); 
-        $labels = array();
+
+        $data = collect([]);
+        $labels = [];
         foreach ($teachersByCountries as $key => $teachersByCountry) {
             $data->push($teachersByCountry->teacher_count);
             $labels[] = $teachersByCountry->country_name;
         }
-        
+
         $ret = new LatestUsers;
         $ret->labels($labels);
         $dataset = $ret->dataset('Teachers by Country', 'bar', $data);
-        
+
         //https://www.chartjs.org/docs/latest/charts/line.html
         $dataset->options([
             'borderColor' => '#2669A0',
         ]);
-    
+
         return $ret;
     }
-    
+
     /***************************************************************************/
+
     /**
-     * Create a BAR chart showing the number of Organizers by country
+     * Create a BAR chart showing the number of Organizers by country.
      *
      * @return App\Charts
      */
-    
-    public function createOrganizersByCountriesChart(){
-    
+    public function createOrganizersByCountriesChart()
+    {
         $organizersByCountries = Organizer::
                         leftJoin('countries', 'organizers.country_id', '=', 'countries.id')
                         ->select(DB::raw('count(*) as organizer_count, countries.name as country_name'))
@@ -186,35 +182,36 @@ class StatisticsController extends Controller
                         ->orderBy('country_name')
                         ->get();
         //dd($organizersByCountries);
-        $data = collect([]); 
-        $labels = array();
+        $data = collect([]);
+        $labels = [];
         foreach ($teachersByCountries as $key => $teachersByCountry) {
             $data->push($teachersByCountry->teacher_count);
             $labels[] = $teachersByCountry->country_name;
         }
-        
+
         $ret = new LatestUsers;
         $ret->labels($labels);
         $dataset = $ret->dataset('Teachers by Country', 'bar', $data);
-        
+
         //https://www.chartjs.org/docs/latest/charts/line.html
         $dataset->options([
             'borderColor' => '#2669A0',
         ]);
-    
+
         return $ret;
     }
-    
+
     /***************************************************************************/
+
     /**
-     * Create a BAR chart showing the number of Events by country
+     * Create a BAR chart showing the number of Events by country.
      *
      * @return App\Charts
      */
-    
-    public function createEventsByCountriesChart(){
+    public function createEventsByCountriesChart()
+    {
         //$eventsByCountries = Event::getActiveEvents();
-        
+
         $activeEvents = Event::getEvents(null, null, null, null, null, null, null, null, null, 10000);
         $grouped = $activeEvents->groupBy(function ($item, $key) {
             return $item['sc_country_name'];
@@ -223,18 +220,18 @@ class StatisticsController extends Controller
             return collect($item)->count();
         });
         $eventsByCountries = $eventsByCountries->sortKeys();
-        
-        $data = collect([]); 
-        $labels = array();
+
+        $data = collect([]);
+        $labels = [];
         foreach ($eventsByCountries as $key => $eventsByCountry) {
             $data->push($eventsByCountry);
             $labels[] = $key;
         }
-        
+
         $ret = new LatestUsers;
         $ret->labels($labels);
         $dataset = $ret->dataset('Events by Country', 'bar', $data);
-        
+
         //https://www.chartjs.org/docs/latest/charts/line.html
         $dataset->options([
             'borderColor' => '#2669A0',
@@ -242,8 +239,4 @@ class StatisticsController extends Controller
 
         return $ret;
     }
-    
-    
-    
-
 }
