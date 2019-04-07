0d0dca8873cc9259654015310ba1768b956adeea
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-07 14:56:58 +0200

    fixed sitemap index

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,18 +1,5 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    {{--<sitemap>
-        <loc>https://laravel-news.com/sitemap/posts</loc>
-        <lastmod>{{Carbon\Carbon::parse($post->updated_at)->format('Y-m-d')}}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>https://laravel-news.com/sitemap/categories</loc>
-        <lastmod>{{Carbon\Carbon::parse($post->updated_at)->format('Y-m-d')}}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>https://laravel-news.com/sitemap/podcasts</loc>
-        <lastmod>{{Carbon\Carbon::parse($event->updated_at)->format('Y-m-d')}}</lastmod>
-    </sitemap>--}}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($menuItems as $menuItem)
         <url>
             @if($menuItem->type == 2)
@@ -25,4 +12,4 @@
             <priority>0.6</priority>
         </url>
     @endforeach
-</sitemapindex>
+</urlset>
