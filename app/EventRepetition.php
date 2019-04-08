1f96de264118737bb5d114f794b933568d1715f4
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-08 18:29:19 +0200

    Apply fixes from StyleCI (#95)

--- app/EventRepetition.php
+++ app/EventRepetition.php
@@ -30,7 +30,7 @@ class EventRepetition extends Model
     /**
      * Get for each event the first event repetition in the near future (JUST THE QUERY to use as SUBQUERY).
      * Parameters are Start date and End date of the interval
-     * Return the query string, 
+     * Return the query string,.
      * @param  string $searchStartDate
      * @param  string $searchStartDate
      * @return string $searchEndDate
