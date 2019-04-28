941081a4025c527f0284b5f7f2d72a155b821c2b
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-28 15:53:37 +0200

    refactored code

--- resources/views/partials/forms/checkbox.blade.php
+++ resources/views/partials/forms/checkbox.blade.php
@@ -9,8 +9,8 @@
 
 <div class="form-group">
     <div class="custom-control custom-checkbox">
-        <input type="checkbox" name="{{$name}}" class="custom-control-input{{ $errors->has($name) ? ' is-invalid' : '' }}" id="customCheck1" @if($value) checked @endif >
-        <label class="custom-control-label" for="customCheck1">{{$description}}</label>
+        <input type="checkbox" name="{{$name}}" class="custom-control-input{{ $errors->has($name) ? ' is-invalid' : '' }}" id="{{$name}}" @if($value) checked @endif >
+        <label class="custom-control-label" for="{{$name}}">{{$description}}</label>
     </div>
     @if ($errors->has($name))
         <span class="invalid-feedback" role="alert">
