34bb0c6ecf8f9fb764bb8ec8a689865f4e1f1b1a
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-20 15:27:16 +0200

    added migration for gift table

--- database/migrations/2019_06_20_111137_add_column_to_donations_offers.php
+++ database/migrations/2019_06_20_111137_add_column_to_donations_offers.php
@@ -14,13 +14,13 @@ class AddColumnToDonationsOffers extends Migration
     public function up()
     {
         Schema::table('donation_offers', function (Blueprint $table) {
-            $table->string('economic_value')->nullable();
-            $table->string('estimated_volunteer_time')->nullable();
+            $table->string('gift_donater')->nullable();
+            $table->string('gift_economic_value')->nullable();
+            $table->string('gift_volunteer_time_value')->nullable();
             $table->string('gift_given_to')->nullable();
-            $table->string('gift_given_when')->nullable();
-            $table->string('country_of_the_gift')->nullable();
-            $table->string('admin_notes')->nullable();
-            
+            $table->dateTime('gift_given_when')->nullable();
+            $table->integer('gift_country_of')->nullable();
+            $table->text('admin_notes')->nullable();
         });
     }
 
@@ -32,7 +32,13 @@ class AddColumnToDonationsOffers extends Migration
     public function down()
     {
         Schema::table('donation_offers', function (Blueprint $table) {
-            //
+            $table->dropColumn('gift_donater');
+            $table->dropColumn('gift_economic_value');
+            $table->dropColumn('gift_volunteer_time_value');
+            $table->dropColumn('gift_given_to');
+            $table->dropColumn('gift_given_when');
+            $table->dropColumn('gift_country_of');
+            $table->dropColumn('admin_notes');
         });
     }
 }
