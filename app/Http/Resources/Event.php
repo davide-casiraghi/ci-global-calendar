bb423805520dac9aacf52a1ac853aa2018fd4be2
Artur Komarov <artur.komarov@gmail.com>
2019-06-23 16:39:51 +0800

    Add more event fields for api

--- app/Http/Resources/Event.php
+++ app/Http/Resources/Event.php
@@ -16,6 +16,8 @@ class Event extends JsonResource
      */
     public function toArray($request)
     {
+        $venue = EventVenue::find($this->venue_id);
+
         return [
             'id' => $this->id,
             'title' => $this->title,
@@ -24,7 +26,17 @@ class Event extends JsonResource
             'facebook_event_link' => $this->facebook_event_link,
             'category' => EventCategory::getCategoryName($this->category_id),
             'teachers' => $this->teachers()->pluck('name', 'id'),
-            'venue' => EventVenue::getVenueName($this->venue_id),
+            'venue' => $venue->name,
+            'start_repeat' => $this->start_repeat,
+            'end_repeat' => $this->end_repeat,
+            'image' => $this->image,
+            'contact_email' => $this->contact_email,
+            'repeat_type' => $this->repeat_type,
+            'repeat_until' => $this->repeat_until,
+            'repeat_monthly_on' => $this->repeat_monthly_on,
+            'city' => $this->sc_city_name,
+            'venue_address' => $venue->address,
+            'zip_code' => $venue->zip_code,
         ];
     }
 }
