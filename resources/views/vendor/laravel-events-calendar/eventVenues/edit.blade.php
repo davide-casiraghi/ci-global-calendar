87c77c216730175b9d021f0362b648d0be59a9e1
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-08-28 14:33:43 +0200

    fixed selected attributes in create and edit views

--- resources/views/vendor/laravel-events-calendar/eventVenues/edit.blade.php
+++ resources/views/vendor/laravel-events-calendar/eventVenues/edit.blade.php
@@ -36,8 +36,7 @@
                             'name' => 'created_by',
                             'placeholder' => __('laravel-events-calendar.select_owner'),
                             'records' => $users,
-                            'selected
-' => $eventVenue->created_by,
+                            'selected' => $eventVenue->created_by,
                             'liveSearch' => 'true',
                             'mobileNativeMenu' => false,
                             'required' => false,
