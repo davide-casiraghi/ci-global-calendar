29d32dde27c57d948736ac4b231819750ad58241
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-09-11 09:06:33 +0200

    now users export show also the country

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -12,6 +12,7 @@ class UsersExport implements FromCollection
      */
     public function collection()
     {
-        return User::select('id', 'name', 'email', 'created_at', 'description')->get();
+        return User::join('countries', 'users.country_id', '=', 'countries.id')
+                    ->select('users.id', 'users.name as userName', 'countries.name as countryName', 'users.email', 'users.created_at', 'users.description')->get();
     }
 }
