4182207e908487a4d1908c68fe32249fe98fe17e
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-03-15 16:13:36 +0100

    added user lat and lng

--- config/leaflet.php
+++ config/leaflet.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'zoom_level'           => 3,
+    'zoom_level'           => 6,
     'detail_zoom_level'    => 13,
     'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-3.313695'),
     'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '114.590148'),
