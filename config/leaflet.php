1d4a7633c03e25178b6d7f15d3b57084a78a4baf
Davide Casiraghi <davide.casiraghi@gmail.com>
2020-03-15 16:13:57 +0100

    responsive background image index view

--- config/leaflet.php
+++ config/leaflet.php
@@ -2,7 +2,7 @@
 
 return [
     'zoom_level'           => 6,
-    'detail_zoom_level'    => 13,
+    'detail_zoom_level'    => 16,
     'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-3.313695'),
     'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '114.590148'),
 ];
